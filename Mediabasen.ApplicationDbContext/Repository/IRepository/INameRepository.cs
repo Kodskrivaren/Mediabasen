72bf14b59a9243c23b488bc9763347c31962bea3
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-06-30 18:55:32 +0200

    added repository for names

---
+++ Mediabasen.ApplicationDbContext/Repository/IRepository/INameRepository.cs
@@ -0,0 +1,9 @@
+﻿using Mediabasen.Models.Product;
+
+namespace Mediabasen.DataAccess.Repository.IRepository
+{
+    public interface INameRepository : IRepository<Name>
+    {
+        public void Update(Name productMovie);
+    }
+}
---
+++ Mediabasen.ApplicationDbContext/Repository/NameRepository.cs
@@ -0,0 +1,20 @@
+﻿using Mediabasen.DataAccess.Data;
+using Mediabasen.DataAccess.Repository.IRepository;
+using Mediabasen.Models.Product;
+
+namespace Mediabasen.DataAccess.Repository
+{
+    public class NameRepository : Repository<Name>, INameRepository
+    {
+        private ApplicationDbContext _db;
+        public NameRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Name product)
+        {
+            _db.Names.Update(product);
+        }
+    }
+}
