6431654731c1f807e958a8eb90f2dded3bfd0ae5
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-06-30 13:45:44 +0200

    created unit of work for accessing repositories

---
+++ Mediabasen.ApplicationDbContext/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace Mediabasen.DataAccess.Repository.IRepository
+{
+    public interface IUnitOfWork
+    {
+        public IProductRepository Product { get; }
+        void Save();
+    }
+}
---
+++ Mediabasen.ApplicationDbContext/Repository/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using Mediabasen.DataAccess.Data;
+using Mediabasen.DataAccess.Repository.IRepository;
+
+namespace Mediabasen.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        ApplicationDbContext _db;
+        public IProductRepository Product { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Product = new ProductRepository(_db);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
