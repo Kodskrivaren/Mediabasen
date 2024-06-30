038696dfe4cc3475325e327d52bc860c3622728c
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-06-30 19:41:22 +0200

    repository for movie actors

---
+++ Mediabasen.ApplicationDbContext/Repository/IRepository/IMovieActorRepository.cs
@@ -0,0 +1,9 @@
+﻿using Mediabasen.Models.Product.Movie;
+
+namespace Mediabasen.DataAccess.Repository.IRepository
+{
+    public interface IMovieActorRepository : IRepository<MovieActor>
+    {
+        public void Update(MovieActor movieActors);
+    }
+}
---
+++ Mediabasen.ApplicationDbContext/Repository/MovieActorRepository.cs
@@ -0,0 +1,20 @@
+﻿using Mediabasen.DataAccess.Data;
+using Mediabasen.DataAccess.Repository.IRepository;
+using Mediabasen.Models.Product.Movie;
+
+namespace Mediabasen.DataAccess.Repository
+{
+    public class MovieActorRepository : Repository<MovieActor>, IMovieActorRepository
+    {
+        private ApplicationDbContext _db;
+        public MovieActorRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(MovieActor movieActors)
+        {
+            _db.MoviesActors.Update(movieActors);
+        }
+    }
+}
