555cfe0873485c98706f66ce4f51424c6514563b
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-06-30 18:53:09 +0200

    added more properties to a user

--- Mediabasen.Models/ApplicationUser.cs
+++ Mediabasen.Models/ApplicationUser.cs
@@ -1,8 +1,18 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Mediabasen.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Adress { get; set; }
+        [Required]
+        public string PostalCode { get; set; }
+        [Required]
+        public string City { get; set; }
+
     }
 }
