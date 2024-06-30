282f0db8c6d51b52258e3c1223f17c5f9bcfd55a
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-06-30 19:43:18 +0200

    controller for handling names

---
+++ Mediabasen.Server/Controllers/NameController.cs
@@ -0,0 +1,28 @@
+﻿using Mediabasen.DataAccess.Repository.IRepository;
+using Mediabasen.Models.Product;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mediabasen.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public NameController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpPost]
+        public IActionResult AddName(Name name)
+        {
+            if (!ModelState.IsValid) return new JsonResult(new { message = "Invalid name!" });
+
+            _unitOfWork.Name.Add(name);
+            _unitOfWork.Save();
+
+            return new JsonResult(new { message = "Name added!" });
+        }
+    }
+}
