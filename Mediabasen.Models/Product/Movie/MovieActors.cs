8d4e3998c75b782e25dca3d6ed1cb5c7109753ed
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-06-30 18:52:06 +0200

    added models for movies

---
+++ Mediabasen.Models/Product/Movie/MovieActors.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mediabasen.Models.Product.Movie
+{
+    public class MovieActors
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int ProductMovieId { get; set; }
+        [ForeignKey("ProductMovieId")]
+        public ProductMovie Movie { get; set; }
+        [Required]
+        public int NameId { get; set; }
+        [ForeignKey("NameId")]
+        public Name Actor { get; set; }
+    }
+}
---
+++ Mediabasen.Models/Product/Movie/ProductMovie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mediabasen.Models.Product.Movie
+{
+    public class ProductMovie : Product
+    {
+        public int DirectorNameId { get; set; }
+        [ForeignKey("DirectorNameId")]
+        [NotMapped]
+        public Name Director { get; set; }
+        [NotMapped]
+        List<Name> Actors { get; set; } = new List<Name>();
+    }
+}
