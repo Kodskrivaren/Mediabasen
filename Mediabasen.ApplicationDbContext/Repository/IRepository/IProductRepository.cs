6a5a9b41de623bcabe8f312299f1bc227870aceb
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-06-30 13:45:19 +0200

    repository for product

---
+++ Mediabasen.ApplicationDbContext/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using Mediabasen.Models;
+
+namespace Mediabasen.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        public void Update(Product product);
+    }
+}
---
+++ Mediabasen.ApplicationDbContext/Repository/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using Mediabasen.DataAccess.Data;
+using Mediabasen.DataAccess.Repository.IRepository;
+using Mediabasen.Models;
+
+namespace Mediabasen.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product product)
+        {
+            _db.Products.Update(product);
+        }
+    }
+}
