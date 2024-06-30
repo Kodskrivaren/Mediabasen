fb035cf5e80d0eaa550c2244b4f5b477c8e004d5
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-06-30 13:44:26 +0200

    created application db context

---
+++ Mediabasen.ApplicationDbContext/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Mediabasen.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mediabasen.DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
