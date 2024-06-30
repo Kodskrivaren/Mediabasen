4ba9ef9322b932eeae01a2ad8ad04d62455cd13a
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-06-30 13:46:51 +0200

    base controller for product

---
+++ Mediabasen.Server/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Mediabasen.DataAccess.Repository.IRepository;
+using Mediabasen.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mediabasen.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Products()
+        {
+            List<Product> products = _unitOfWork.Product.GetAll().ToList();
+            JsonResult res = new JsonResult(new { products });
+            return res;
+        }
+    }
+}
