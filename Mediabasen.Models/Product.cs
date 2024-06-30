e8935f57951aaf158e055f1ca92f4fee3e0daa25
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-06-30 13:43:30 +0200

    model for product

---
+++ Mediabasen.Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mediabasen.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
