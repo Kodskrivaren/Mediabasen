b663525556fb8ec07b7271680cebf942e1a07f9e
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-06-30 20:50:08 +0200

    config for postcss and tailwind

---
+++ mediabasen.client/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#000000",
+        bright: "#ffffff",
+        accent: "#57A2EF",
+        dark: "#24272B",
+        light: "#4A525A",
+        middle: "#404040",
+      },
+      screens: {},
+    },
+  },
+  plugins: [import("flowbite/plugin")],
+};
