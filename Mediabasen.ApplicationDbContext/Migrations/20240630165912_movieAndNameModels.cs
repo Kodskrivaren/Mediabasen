080a73e28422153e7124682afcc74c4aa3b04b61
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-06-30 18:59:39 +0200

    db migration for movies and names

---
+++ Mediabasen.ApplicationDbContext/Migrations/20240630165912_movieAndNameModels.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Mediabasen.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class movieAndNameModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DirectorNameId",
+                table: "Products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Discount",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Products",
+                type: "varchar(13)",
+                maxLength: 13,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Names",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Fullname = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Names", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Names");
+
+            migrationBuilder.DropColumn(
+                name: "DirectorNameId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Discount",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Products");
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "Name", "Price" },
+                values: new object[] { 1, "Horror Movie", "Kill List", 79m });
+        }
+    }
+}
