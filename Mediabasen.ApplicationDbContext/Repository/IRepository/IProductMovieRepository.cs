e90ab6304bb99cbbe82306f490bfec27d8b0ebfd
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-06-30 18:55:18 +0200

    added repository for movies

---
+++ Mediabasen.ApplicationDbContext/Repository/IRepository/IProductMovieRepository.cs
@@ -0,0 +1,9 @@
+﻿using Mediabasen.Models.Product.Movie;
+
+namespace Mediabasen.DataAccess.Repository.IRepository
+{
+    public interface IProductMovieRepository : IRepository<ProductMovie>
+    {
+        public void Update(ProductMovie productMovie);
+    }
+}
---
+++ Mediabasen.ApplicationDbContext/Repository/ProductMovieRepository.cs
@@ -0,0 +1,20 @@
+﻿using Mediabasen.DataAccess.Data;
+using Mediabasen.DataAccess.Repository.IRepository;
+using Mediabasen.Models.Product.Movie;
+
+namespace Mediabasen.DataAccess.Repository
+{
+    public class ProductMovieRepository : Repository<ProductMovie>, IProductMovieRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductMovieRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(ProductMovie product)
+        {
+            _db.ProductMovies.Update(product);
+        }
+    }
+}
