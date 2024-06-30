b1b6de815220435a85a21a0138c1c4367a398f85
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-06-30 18:52:55 +0200

    models for product images, names and genres

---
+++ Mediabasen.Models/Product/Name.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mediabasen.Models.Product
+{
+    public class Name
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Fullname { get; set; }
+    }
+}
---
+++ Mediabasen.Models/Product/ProductImage.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mediabasen.Models.Product
+{
+    public class ProductImage
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+    }
+}
