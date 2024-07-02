02edf5f2fa0e0709eb71bd87aa5a96c7bba255d0
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-02 20:49:15 +0200

    added logos and styling

--- mediabasen.client/src/components/globals/Header.jsx
+++ mediabasen.client/src/components/globals/Header.jsx
@@ -1,21 +1,17 @@
-import { List, ListItem } from "flowbite-react";
-import { Link } from "react-router-dom";
+import CartLogo from "./../../assets/icons/cart-outline.svg?react";
+import UserLogo from "./../../assets/icons/person-outline.svg?react";
 
 export default function Header() {
   return (
-    <header className="flex justify-around pt-10 pb-10 bg-dark text-bright">
-      <List className="flex gap-5">
-        <ListItem>
-          <Link to={"/"}>Hem</Link>
-        </ListItem>
-        <ListItem>
-          <a>Kontakt</a>
-        </ListItem>
-      </List>
-      <h1>Mediabasen</h1>
+    <header className="flex justify-between p-3 bg-dark text-bright">
+      <h1 className="text-2xl font-bold">Mediabasen</h1>
       <ul className="flex gap-5">
-        <li>Cart</li>
-        <li>Profile</li>
+        <li>
+          <CartLogo className="w-8 -scale-x-100" />
+        </li>
+        <li>
+          <UserLogo className="w-8" />
+        </li>
       </ul>
     </header>
   );
