2d76bce4b37857a5ef5d4091c0dc6f8efcee490e
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-02 20:49:28 +0200

    added ability to add classes and change color

--- mediabasen.client/src/components/globals/Button.jsx
+++ mediabasen.client/src/components/globals/Button.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 
 export default function Button(props) {
+  const { className, classNameColor } = props;
+
   return (
-    <button className="bg-dark w-20 text-bright p-2 rounded-lg">
+    <button
+      className={`${
+        classNameColor ? classNameColor : "bg-dark"
+      } w-20 text-bright p-2 rounded-lg hover:bg-middle transition-colors duration-200${
+        className ? ` ${className}` : ""
+      }`}>
       {props.children}
     </button>
   );
