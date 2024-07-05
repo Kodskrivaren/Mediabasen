f4c435898de4befd85e2e5027766121bd5a364ce
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-05 21:31:51 +0200

    added type and onclick properties

--- mediabasen.client/src/components/globals/Button.jsx
+++ mediabasen.client/src/components/globals/Button.jsx
@@ -9,7 +9,9 @@ export default function Button(props) {
         classNameColor ? classNameColor : "bg-dark"
       } w-20 text-bright p-2 rounded-lg hover:bg-middle transition-colors duration-200${
         className ? ` ${className}` : ""
-      }`}>
+      }`}
+      type={props.type ? props.type : "button"}
+      onClick={props.onClick}>
       {props.children}
     </button>
   );
