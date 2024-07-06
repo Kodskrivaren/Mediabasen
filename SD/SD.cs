e33d728fc76f1bbab7cb7a8234de9c1aa2374f18
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-06 18:54:51 +0200

    added constants for roles

---
+++ SD/SD.cs
@@ -0,0 +1,8 @@
+﻿namespace Mediabasen.Utility.SD
+{
+    public static class SD
+    {
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+    }
+}
