620942c71ca13c7c42a5669ce2bff56bd8d15da6
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-06 12:21:02 +0200

    model for creating an account

---
+++ Mediabasen.Models/ControllerModels/NewAccount.cs
@@ -0,0 +1,12 @@
+﻿namespace Mediabasen.Models.ControllerModels
+{
+    public class NewAccount
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Adress { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+    }
+}
