b39f91dda4b6b26c9c0d5e1d6a516136286e02ff
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-06 10:59:56 +0200

    added options

--- mediabasen.client/src/utils/fetchHelper.js
+++ mediabasen.client/src/utils/fetchHelper.js
@@ -1,13 +1,15 @@
-export default function fetchHelper(url, method, body) {
+export default function fetchHelper(url, method, body, noContentType) {
   const fetchOptions = {
     headers: {},
     method,
   };
 
   if (method !== "GET") {
-    fetchOptions.body = JSON.stringify(body);
-    fetchOptions.headers["Content-Type"] = "application/json";
+    fetchOptions.body = noContentType ? body : JSON.stringify(body);
+    fetchOptions.headers["Content-Type"] = noContentType
+      ? undefined
+      : "application/json";
   }
-
+  console.log(fetchOptions);
   return fetch("/api" + url, fetchOptions);
 }
