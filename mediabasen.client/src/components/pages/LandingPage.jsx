33696b0cdfd932d693309782b414a9bb4396631f
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-06 11:52:02 +0200

    base component for landing page

---
+++ mediabasen.client/src/components/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import CategoryPicker from "../globals/CategoryPicker";
+import NewProducts from "./LandingPage/NewProducts";
+
+export default function LandingPage() {
+  return (
+    <>
+      <CategoryPicker />
+      <NewProducts />
+    </>
+  );
+}
