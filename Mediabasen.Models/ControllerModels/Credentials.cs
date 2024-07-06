89fb6436c0de2ea60f456887acd11111f068ae7e
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-06 12:38:39 +0200

    model for authenticating with the server

---
+++ Mediabasen.Models/ControllerModels/Credentials.cs
@@ -0,0 +1,8 @@
+﻿namespace Mediabasen.Models.ControllerModels
+{
+    public class Credentials
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
