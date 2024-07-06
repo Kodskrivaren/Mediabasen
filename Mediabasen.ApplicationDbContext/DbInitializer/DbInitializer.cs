c71bc3cb36b67a4e046e0e221784a6e2c5691b57
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-06 19:53:44 +0200

    added db initializer for adding roles and migrate pending migrations

---
+++ Mediabasen.ApplicationDbContext/DbInitializer/DbInitializer.cs
@@ -0,0 +1,37 @@
+﻿using Mediabasen.DataAccess.Data;
+using Mediabasen.Utility.SD;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mediabasen.DataAccess.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _db;
+        public DbInitializer(RoleManager<IdentityRole> roleManager, ApplicationDbContext db)
+        {
+            _db = db;
+            _roleManager = roleManager;
+        }
+
+        public void Initialize(WebApplicationBuilder builder)
+        {
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception ex) { }
+
+            if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+            }
+        }
+    }
+}
---
+++ Mediabasen.ApplicationDbContext/DbInitializer/IDbInitializer.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Mediabasen.DataAccess.DbInitializer
+{
+    public interface IDbInitializer
+    {
+        void Initialize(WebApplicationBuilder builder);
+    }
+}
