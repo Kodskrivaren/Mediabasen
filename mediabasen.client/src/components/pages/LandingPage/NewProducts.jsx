3d119de1c5de7105af967795252a0a500e298d9a
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-06 11:51:51 +0200

    fetch movies from api

--- mediabasen.client/src/components/pages/LandingPage/NewProducts.jsx
+++ mediabasen.client/src/components/pages/LandingPage/NewProducts.jsx
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ProductCard from "../../globals/ProductCard";
+import movieService from "../../../services/movieService";
 
 export default function NewProducts() {
-  const liClasses = "inline-block relative w-card flex-shrink-0";
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const result = await movieService.fetchNewMovies();
+
+      setMovies(result.products);
+    }
+
+    fetchMovies();
+  }, [setMovies]);
 
   return (
     <section className="bg-middle p-3">
       <h2 className="text-white pb-3 font-bold">Nya Produkter</h2>
       <ul className="flex gap-card w-full overflow-x-scroll flex-row">
-        <li className={liClasses}>
-          <ProductCard />
-        </li>
-        <li className={liClasses}>
-          <ProductCard />
-        </li>
-        <li className={liClasses}>
-          <ProductCard />
-        </li>
+        {movies.map((movie) => (
+          <li
+            key={`movie-${movie.id}`}
+            className="inline-block relative w-card flex-shrink-0">
+            <ProductCard product={movie} />
+          </li>
+        ))}
       </ul>
     </section>
   );
