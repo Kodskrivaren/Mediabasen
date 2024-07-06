ec467c90002b29c5fb471f88e81cd095d0d13c16
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-06 11:00:49 +0200

    component for listing added entities

---
+++ mediabasen.client/src/components/pages/Admin/AddedList.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Button from "../../globals/Button";
+
+export default function AddedList({
+  title,
+  nothingAddedText,
+  entityDisplayProperty,
+  list,
+  setList,
+}) {
+  return (
+    <>
+      <p className="text-white">{title}:</p>
+      <ul className="flex flex-col gap-4 overflow-y-scroll h-40 rounded bg-dark p-3">
+        {list.length === 0 && (
+          <li>
+            <p className="text-white">{nothingAddedText}</p>
+          </li>
+        )}
+        {list.map((listItem) => (
+          <li className="text-white flex flex-row justify-between">
+            <p>{listItem[entityDisplayProperty]}</p>
+            <Button
+              classNameColor="bg-red-500"
+              onClick={() => setList(list.filter((item) => item !== listItem))}>
+              Ta bort
+            </Button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
