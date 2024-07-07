db296ac504adb7aa075d22b5497e0c8dbaf49f68
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-07 09:53:41 +0200

    added route for login and user context

--- mediabasen.client/src/App.jsx
+++ mediabasen.client/src/App.jsx
@@ -1,21 +1,29 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/globals/Footer";
 import Header from "./components/globals/Header";
 import LandingPage from "./components/pages/LandingPage";
 import AdminPage from "./components/pages/Admin/AdminPage";
+import LoginPage from "./components/pages/User/LoginPage";
+import UserContext from "./contexts/UserContext";
 
 function App() {
+  const [user, setUser] = useState();
+
   return (
-    <BrowserRouter>
-      <Header />
-      <main className="flex-grow">
-        <Routes>
-          <Route index element={<LandingPage />} />
-          <Route path="/admin/*" element={<AdminPage />} />
-        </Routes>
-      </main>
-      <Footer />
-    </BrowserRouter>
+    <UserContext.Provider value={{ user, setUser }}>
+      <BrowserRouter>
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route index element={<LandingPage />} />
+            <Route path="/admin/*" element={<AdminPage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
