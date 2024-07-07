5cecde028b7da9a8bcfd10920f327949fec974c5
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-07 09:54:42 +0200

    added link for login

--- mediabasen.client/src/components/globals/Header.jsx
+++ mediabasen.client/src/components/globals/Header.jsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
 import CartLogo from "./../../assets/icons/cart-outline.svg?react";
 import UserLogo from "./../../assets/icons/person-outline.svg?react";
+import { Link } from "react-router-dom";
+import UserContext from "../../contexts/UserContext";
 
 export default function Header() {
+  const userContext = useContext(UserContext);
+
   return (
     <header className="flex justify-between p-3 bg-dark text-bright">
       <h1 className="text-2xl font-bold">Mediabasen</h1>
@@ -10,7 +15,16 @@ export default function Header() {
           <CartLogo className="w-8 -scale-x-100" />
         </li>
         <li>
-          <UserLogo className="w-8" />
+          <Link
+            to={userContext.user ? "/user" : "/login"}
+            className="text-center">
+            <UserLogo className="w-8 mx-auto" />
+            <p className="text-sm">
+              {userContext.user
+                ? userContext.user.name.split(" ")[0]
+                : "Logga in"}
+            </p>
+          </Link>
         </li>
       </ul>
     </header>
