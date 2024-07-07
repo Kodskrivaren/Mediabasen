4426730e87456bb9f11bf1a4b4578229b29ac27b
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-07 20:03:05 +0200

    added lazy loading of components

--- mediabasen.client/src/App.jsx
+++ mediabasen.client/src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from "react";
+import { lazy, Suspense, useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/globals/Footer";
 import Header from "./components/globals/Header";
-import LandingPage from "./components/pages/LandingPage";
-import AdminPage from "./components/pages/Admin/AdminPage";
-import LoginPage from "./components/pages/User/LoginPage";
+const LandingPage = lazy(() => import("./components/pages/LandingPage"));
+const AdminPage = lazy(() => import("./components/pages/Admin/AdminPage"));
+const LoginPage = lazy(() => import("./components/pages/Auth/LoginPage"));
+const RegisterPage = lazy(() => import("./components/pages/Auth/RegisterPage"));
+const UserPage = lazy(() => import("./components/pages/User/UserPage"));
 import UserContext from "./contexts/UserContext";
+import userService from "./services/userService";
 
 function App() {
   const [user, setUser] = useState();
+
+  useEffect(() => {
+    async function tryGetUserDetails() {
+      const result = await userService.getUserDetails();
+
+      if (result) {
+        setUser(result);
+      }
+    }
+
+    tryGetUserDetails();
+  }, []);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
@@ -18,7 +33,30 @@ function App() {
           <Routes>
             <Route index element={<LandingPage />} />
             <Route path="/admin/*" element={<AdminPage />} />
-            <Route path="/login" element={<LoginPage />} />
+            <Route
+              path="/login"
+              element={
+                <Suspense>
+                  <LoginPage />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <Suspense>
+                  <RegisterPage />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/user/*"
+              element={
+                <Suspense>
+                  <UserPage />
+                </Suspense>
+              }
+            />
           </Routes>
         </main>
         <Footer />
