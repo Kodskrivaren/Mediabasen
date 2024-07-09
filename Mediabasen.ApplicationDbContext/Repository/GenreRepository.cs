9c2ef4432a345af07fd7d19678a40924abfcb7a8
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-09 19:06:30 +0200

    repository for genre

---
+++ Mediabasen.ApplicationDbContext/Repository/GenreRepository.cs
@@ -0,0 +1,20 @@
+﻿using Mediabasen.DataAccess.Data;
+using Mediabasen.DataAccess.Repository.IRepository;
+using Mediabasen.Models.Product;
+
+namespace Mediabasen.DataAccess.Repository
+{
+    public class GenreRepository : Repository<Genre>, IGenreRepository
+    {
+        private ApplicationDbContext _db;
+        public GenreRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Genre genre)
+        {
+            _db.Genres.Update(genre);
+        }
+    }
+}
---
+++ Mediabasen.ApplicationDbContext/Repository/IRepository/IGenreRepository.cs
@@ -0,0 +1,9 @@
+﻿using Mediabasen.Models.Product;
+
+namespace Mediabasen.DataAccess.Repository.IRepository
+{
+    public interface IGenreRepository : IRepository<Genre>
+    {
+        public void Update(Genre genres);
+    }
+}
