63e142d6ef6c6686a39f79c15dd5d3b492b587b5
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-09 19:07:41 +0200

    added model for product genre

---
+++ Mediabasen.Models/Product/ProductGenre.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mediabasen.Models.Product
+{
+    public class ProductGenre
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int GenreId { get; set; }
+        [ForeignKey("GenreId")]
+        public Genre Genre { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+    }
+}
