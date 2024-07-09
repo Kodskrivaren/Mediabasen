9f75ba8dfb7dbc3c396accd0801ef8b269dd0e61
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-09 19:07:13 +0200

    added validation attributes

--- Mediabasen.Models/ControllerModels/NewAccount.cs
+++ Mediabasen.Models/ControllerModels/NewAccount.cs
@@ -4,13 +4,21 @@ namespace Mediabasen.Models.ControllerModels
 {
     public class NewAccount
     {
+        [Required(AllowEmptyStrings = false)]
         public string Name { get; set; }
+        [Required(AllowEmptyStrings = false)]
         [EmailAddress]
         public string Email { get; set; }
+        [Required(AllowEmptyStrings = false)]
         public string Password { get; set; }
+        [Required(AllowEmptyStrings = false)]
         public string Adress { get; set; }
-        public string PostalCode { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        public int PostalCode { get; set; }
+        [Required(AllowEmptyStrings = false)]
         public string City { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [Phone]
         public string PhoneNumber { get; set; }
     }
 }
