d41910646c5b5b02e976cadb0863336f3c9cf423
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-09 19:08:50 +0200

    added authorization attributes

--- Mediabasen.Server/Controllers/NameController.cs
+++ Mediabasen.Server/Controllers/NameController.cs
@@ -1,10 +1,12 @@
 ﻿using Mediabasen.DataAccess.Repository.IRepository;
 using Mediabasen.Models.Product;
+using Mediabasen.Utility.SD;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mediabasen.Server.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class NameController : ControllerBase
     {
@@ -15,6 +17,8 @@ namespace Mediabasen.Server.Controllers
         }
 
         [HttpGet]
+        [ActionName("FindName")]
+        [Authorize(Roles = SD.Role_Admin)]
         public IActionResult FindName(string query)
         {
             var foundNames = _unitOfWork.Name.GetAll(u => u.Fullname.ToLower().StartsWith(query.ToLower())).ToList();
@@ -23,6 +27,8 @@ namespace Mediabasen.Server.Controllers
         }
 
         [HttpPost]
+        [ActionName("AddName")]
+        [Authorize(Roles = SD.Role_Admin)]
         public IActionResult AddName(Name name)
         {
             if (!ModelState.IsValid) return new JsonResult(new { message = "Invalid name!" });
