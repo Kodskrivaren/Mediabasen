f1c1d5790055c5776559495c1b072505ccc3819e
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-09 19:06:42 +0200

    repository for product genre

---
+++ Mediabasen.ApplicationDbContext/Repository/IRepository/IProductGenreRepository.cs
@@ -0,0 +1,9 @@
+﻿using Mediabasen.Models.Product;
+
+namespace Mediabasen.DataAccess.Repository.IRepository
+{
+    public interface IProductGenreRepository : IRepository<ProductGenre>
+    {
+        public void Update(ProductGenre productGenres);
+    }
+}
---
+++ Mediabasen.ApplicationDbContext/Repository/ProductGenreRepository.cs
@@ -0,0 +1,20 @@
+﻿using Mediabasen.DataAccess.Data;
+using Mediabasen.DataAccess.Repository.IRepository;
+using Mediabasen.Models.Product;
+
+namespace Mediabasen.DataAccess.Repository
+{
+    public class ProductGenreRepository : Repository<ProductGenre>, IProductGenreRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductGenreRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(ProductGenre productGenres)
+        {
+            _db.ProductGenres.Update(productGenres);
+        }
+    }
+}
