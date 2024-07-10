8bfdc1cac4d34ecbf92c624931768ba97e2fcb18
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-10 18:30:58 +0200

    renders out admin links

--- mediabasen.client/src/components/pages/Admin/AdminPage.jsx
+++ mediabasen.client/src/components/pages/Admin/AdminPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import AddMovie from "./AddMovie";
+import AdminLinks from "./AdminLinks";
 
 export default function AdminPage() {
   return (
     <section className="p-3">
       <h2 className="text-white font-bold text-lg">Admin Sida</h2>
       <Routes>
+        <Route path="/" element={<AdminLinks />} />
         <Route path="/addmovie" element={<AddMovie />} />
       </Routes>
     </section>
