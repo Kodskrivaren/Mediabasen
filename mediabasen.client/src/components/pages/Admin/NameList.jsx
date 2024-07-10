1f26fee531f201f20671626d355d7955d131d908
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-10 18:22:32 +0200

    added nameprop option

--- mediabasen.client/src/components/pages/Admin/NameList.jsx
+++ mediabasen.client/src/components/pages/Admin/NameList.jsx
@@ -4,17 +4,20 @@ export default function NameList({
   nameSearchResult,
   onNameClick,
   listKeyPrefix,
+  nameProp = "fullname",
 }) {
   return (
     <div className="-mt-4 relative h-0">
-      <ul className="absolute w-full bg-white hover:bg-slate-300 rounded-b p-2">
+      <ul className="absolute w-full bg-white rounded-b p-2">
         {nameSearchResult.map((name) => (
-          <li key={`${listKeyPrefix}-${name.id}`}>
+          <li
+            key={`${listKeyPrefix}-${name.id}`}
+            className="hover:bg-slate-300">
             <button
               className="w-full h-full p-1"
               type="button"
               onClick={() => onNameClick(name)}>
-              <p className="text-left">{name.fullname}</p>
+              <p className="text-left">{name[nameProp]}</p>
             </button>
           </li>
         ))}
