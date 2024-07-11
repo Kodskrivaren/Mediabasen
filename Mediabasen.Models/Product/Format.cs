69c4f117aac3688c165ae9c7eaaceb3db33e85af
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-11 19:52:42 +0200

    model for format

---
+++ Mediabasen.Models/Product/Format.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mediabasen.Models.Product
+{
+    public class Format
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string Name { get; set; }
+    }
+}
