d5a894755483438378dc57b00aeb17ed1a8bc422
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-11 19:52:17 +0200

    migration for format

---
+++ Mediabasen.ApplicationDbContext/Migrations/20240711164147_addedFormat.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Mediabasen.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedFormat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FormatId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Formats",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Formats", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_FormatId",
+                table: "Products",
+                column: "FormatId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Formats_FormatId",
+                table: "Products",
+                column: "FormatId",
+                principalTable: "Formats",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Formats_FormatId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Formats");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_FormatId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "FormatId",
+                table: "Products");
+        }
+    }
+}
