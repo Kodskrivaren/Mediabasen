473d41ddd3cbf63268c7dc98f6f538d4022fd0b3
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-11 19:52:57 +0200

    added repository for format

---
+++ Mediabasen.ApplicationDbContext/Repository/FormatRepository.cs
@@ -0,0 +1,20 @@
+﻿using Mediabasen.DataAccess.Data;
+using Mediabasen.DataAccess.Repository.IRepository;
+using Mediabasen.Models.Product;
+
+namespace Mediabasen.DataAccess.Repository
+{
+    public class FormatRepository : Repository<Format>, IFormatRepository
+    {
+        private ApplicationDbContext _db;
+        public FormatRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Format format)
+        {
+            _db.Formats.Update(format);
+        }
+    }
+}
---
+++ Mediabasen.ApplicationDbContext/Repository/IRepository/IFormatRepository.cs
@@ -0,0 +1,9 @@
+﻿using Mediabasen.Models.Product;
+
+namespace Mediabasen.DataAccess.Repository.IRepository
+{
+    public interface IFormatRepository : IRepository<Format>
+    {
+        public void Update(Format formats);
+    }
+}
