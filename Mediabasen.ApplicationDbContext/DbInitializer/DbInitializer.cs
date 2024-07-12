a9c8bbab6ac95f76d3c28e7ad4df6215f69b6491
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-12 21:35:30 +0200

    adds product types if they don't exist in db

--- Mediabasen.ApplicationDbContext/DbInitializer/DbInitializer.cs
+++ Mediabasen.ApplicationDbContext/DbInitializer/DbInitializer.cs
@@ -1,4 +1,6 @@
 ﻿using Mediabasen.DataAccess.Data;
+using Mediabasen.DataAccess.Repository.IRepository;
+using Mediabasen.Models.Product;
 using Mediabasen.Utility.SD;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
@@ -10,10 +12,12 @@ namespace Mediabasen.DataAccess.DbInitializer
     {
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly ApplicationDbContext _db;
-        public DbInitializer(RoleManager<IdentityRole> roleManager, ApplicationDbContext db)
+        private readonly IUnitOfWork _unitOfWork;
+        public DbInitializer(RoleManager<IdentityRole> roleManager, ApplicationDbContext db, IUnitOfWork unitOfWork)
         {
             _db = db;
             _roleManager = roleManager;
+            _unitOfWork = unitOfWork;
         }
 
         public void Initialize(WebApplicationBuilder builder)
@@ -31,6 +35,17 @@ namespace Mediabasen.DataAccess.DbInitializer
             {
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+            }
+
+            var test = _unitOfWork.ProductType.GetFirstOrDefault(u => u.Name == SD.Type_Movie);
+
+            if (test == null)
+            {
+                _db.ProductTypes.Add(new ProductType() { Name = SD.Type_Movie });
+                _db.ProductTypes.Add(new ProductType() { Name = SD.Type_CD });
+                _db.ProductTypes.Add(new ProductType() { Name = SD.Type_Game });
+                _db.ProductTypes.Add(new ProductType() { Name = SD.Type_Book });
+                _db.SaveChanges();
             }
         }
     }
