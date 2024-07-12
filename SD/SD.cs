516c47f78d73dc335bb765a7101ce853f578eed9
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-12 21:34:45 +0200

    added constants for product types

--- SD/SD.cs
+++ SD/SD.cs
@@ -4,5 +4,10 @@
     {
         public const string Role_Admin = "Admin";
         public const string Role_Employee = "Employee";
+
+        public const string Type_Movie = "Movie";
+        public const string Type_CD = "CD";
+        public const string Type_Book = "Book";
+        public const string Type_Game = "Game";
     }
 }
