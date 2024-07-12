7995cf0def09403f4e73df18b73126fdc1063945
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-12 21:37:03 +0200

    added service for products

---
+++ mediabasen.client/src/services/productService.js
@@ -0,0 +1,14 @@
+import fetchHelper from "../utils/fetchHelper";
+
+async function getProductById(id) {
+  const response = await fetchHelper(
+    `/Product/GetProductById?productId=${id}`,
+    "GET"
+  );
+
+  if (response.status < 400) {
+    return await response.json();
+  }
+}
+
+export default { getProductById };
