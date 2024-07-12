9505bee4b55442d2f76abe6f8287bda2cf2886b6
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-12 21:34:03 +0200

    added model for product type

---
+++ Mediabasen.Models/Product/ProductType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mediabasen.Models.Product
+{
+    public class ProductType
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
