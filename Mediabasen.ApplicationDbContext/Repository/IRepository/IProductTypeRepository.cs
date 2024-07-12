c8b47fbaae88ced53a886ab7968b1bd0088e1b6c
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-12 21:35:47 +0200

    added repository for product type

---
+++ Mediabasen.ApplicationDbContext/Repository/IRepository/IProductTypeRepository.cs
@@ -0,0 +1,9 @@
+﻿using Mediabasen.Models.Product;
+
+namespace Mediabasen.DataAccess.Repository.IRepository
+{
+    public interface IProductTypeRepository : IRepository<ProductType>
+    {
+        public void Update(ProductType productTypes);
+    }
+}
---
+++ Mediabasen.ApplicationDbContext/Repository/ProductTypeRepository.cs
@@ -0,0 +1,20 @@
+﻿using Mediabasen.DataAccess.Data;
+using Mediabasen.DataAccess.Repository.IRepository;
+using Mediabasen.Models.Product;
+
+namespace Mediabasen.DataAccess.Repository
+{
+    public class ProductTypeRepository : Repository<ProductType>, IProductTypeRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(ProductType productType)
+        {
+            _db.ProductTypes.Update(productType);
+        }
+    }
+}
