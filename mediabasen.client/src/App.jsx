0c3e72294b8b2fcb7365ff571fe7996b1ac320c2
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-13 13:01:57 +0200

    added context for cart and notification

--- mediabasen.client/src/App.jsx
+++ mediabasen.client/src/App.jsx
@@ -10,9 +10,14 @@ const UserPage = lazy(() => import("./components/pages/User/UserPage"));
 const DetailPage = lazy(() => import("./components/pages/Detail/DetailPage"));
 import UserContext from "./contexts/UserContext";
 import userService from "./services/userService";
+import CartContext from "./contexts/CartContext";
+import NotifyContext from "./contexts/NotifyContext";
+import Notification from "./components/globals/Notification";
 
 function App() {
   const [user, setUser] = useState();
+  const [cart, setCart] = useState();
+  const [note, setNote] = useState();
 
   useEffect(() => {
     async function tryGetUserDetails() {
@@ -27,64 +32,69 @@ function App() {
   }, []);
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
-      <BrowserRouter>
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route
-              index
-              element={
-                <Suspense>
-                  <LandingPage />
-                </Suspense>
-              }
-            />
-            <Route
-              path="/admin/*"
-              element={
-                <Suspense>
-                  <AdminPage />
-                </Suspense>
-              }
-            />
-            <Route
-              path="/login"
-              element={
-                <Suspense>
-                  <LoginPage />
-                </Suspense>
-              }
-            />
-            <Route
-              path="/detail/*"
-              element={
-                <Suspense>
-                  <DetailPage />
-                </Suspense>
-              }
-            />
-            <Route
-              path="/register"
-              element={
-                <Suspense>
-                  <RegisterPage />
-                </Suspense>
-              }
-            />
-            <Route
-              path="/user/*"
-              element={
-                <Suspense>
-                  <UserPage />
-                </Suspense>
-              }
-            />
-          </Routes>
-        </main>
-        <Footer />
-      </BrowserRouter>
-    </UserContext.Provider>
+    <NotifyContext.Provider value={{ note, setNote }}>
+      <CartContext.Provider value={{ cart, setCart }}>
+        <UserContext.Provider value={{ user, setUser }}>
+          <BrowserRouter>
+            <Header />
+            {note && <Notification />}
+            <main className="flex-grow">
+              <Routes>
+                <Route
+                  index
+                  element={
+                    <Suspense>
+                      <LandingPage />
+                    </Suspense>
+                  }
+                />
+                <Route
+                  path="/admin/*"
+                  element={
+                    <Suspense>
+                      <AdminPage />
+                    </Suspense>
+                  }
+                />
+                <Route
+                  path="/login"
+                  element={
+                    <Suspense>
+                      <LoginPage />
+                    </Suspense>
+                  }
+                />
+                <Route
+                  path="/detail/*"
+                  element={
+                    <Suspense>
+                      <DetailPage />
+                    </Suspense>
+                  }
+                />
+                <Route
+                  path="/register"
+                  element={
+                    <Suspense>
+                      <RegisterPage />
+                    </Suspense>
+                  }
+                />
+                <Route
+                  path="/user/*"
+                  element={
+                    <Suspense>
+                      <UserPage />
+                    </Suspense>
+                  }
+                />
+              </Routes>
+            </main>
+            <Footer />
+          </BrowserRouter>
+        </UserContext.Provider>
+      </CartContext.Provider>
+    </NotifyContext.Provider>
   );
 }
 
