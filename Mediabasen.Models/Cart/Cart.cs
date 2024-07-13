0e607a93655026c900eb6896f7ec38c18fbb2ba9
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-13 17:16:30 +0200

    Moved cart models

--- Mediabasen.Models/Cart/Cart.cs
+++ Mediabasen.Models/Cart/Cart.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Mediabasen.Models
+namespace Mediabasen.Models.Cart
 {
     public class Cart
     {
