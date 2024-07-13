f47b930e5c8870d736efa2e0416e47ac46d84147
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-13 13:00:25 +0200

    model for cart

---
+++ Mediabasen.Models/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mediabasen.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int Id { get; set; }
+        public List<CartProduct> CartProducts { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [NotMapped]
+        [ForeignKey("UserId")]
+        public ApplicationUser User { get; set; }
+    }
+}
