911f641575b3f9f05e467e17e02093190442ce66
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-13 13:02:43 +0200

    component for notifications

---
+++ mediabasen.client/src/components/globals/Notification.jsx
@@ -0,0 +1,28 @@
+import React, { useContext, useEffect } from "react";
+import NotifyContext from "../../contexts/NotifyContext";
+
+export default function Notification() {
+  const { note, setNote } = useContext(NotifyContext);
+
+  useEffect(() => {
+    let timeoutId;
+
+    if (note) {
+      timeoutId = setTimeout(() => {
+        setNote(undefined);
+      }, 5500);
+    }
+
+    return () => {
+      if (timeoutId) {
+        clearTimeout();
+      }
+    };
+  }, [note]);
+
+  return (
+    <div className="fixed text-white right-3 translate-x-full top-5 bg-middle p-3 rounded animate-note">
+      {note}
+    </div>
+  );
+}
