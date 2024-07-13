58f96166e3254e981a6fde501f4ed8bdf3a9498e
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-13 13:02:13 +0200

    added logic for adding an item to cart

--- mediabasen.client/src/components/globals/AddToCartBtn.jsx
+++ mediabasen.client/src/components/globals/AddToCartBtn.jsx
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useContext } from "react";
 import Button from "./Button";
+import cartService from "../../services/cartService";
+import CartContext from "../../contexts/CartContext";
+import UserContext from "../../contexts/UserContext";
+import { useNavigate } from "react-router-dom";
+import NotifyContext from "../../contexts/NotifyContext";
 
 export default function AddToCartBtn({ product }) {
+  const { user } = useContext(UserContext);
+  const { cart, setCart } = useContext(CartContext);
+  const { setNote } = useContext(NotifyContext);
+
+  const navigate = useNavigate();
+
+  async function onAddClick() {
+    if (!user) return navigate("/login");
+
+    let count = 1;
+
+    if (cart) {
+      const foundProduct = cart.cartProducts.find(
+        (prod) => prod.productId == product.id
+      );
+
+      if (foundProduct) {
+        count = foundProduct.count + 1;
+      }
+    }
+
+    const newCart = await cartService.addProductToCart(product.id, count);
+
+    setCart(newCart);
+
+    setNote(
+      <>
+        <p>"{product.name}" har lagts till i varukorgen!</p>
+      </>
+    );
+  }
+
   return (
-    <Button classNameColor="bg-accent" className="font-bold flex-grow w-full">
+    <Button
+      classNameColor="bg-accent"
+      onClick={onAddClick}
+      className="font-bold flex-grow w-full">
       Lägg i Varukorg
     </Button>
   );
