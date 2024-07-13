2c1e0b186eba1e3fbc00e1d75dc80718e551e989
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-13 13:02:27 +0200

    added logic for fetching current cart

--- mediabasen.client/src/components/globals/Header.jsx
+++ mediabasen.client/src/components/globals/Header.jsx
@@ -1,18 +1,38 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import CartLogo from "./../../assets/icons/cart-outline.svg?react";
 import UserLogo from "./../../assets/icons/person-outline.svg?react";
 import { Link } from "react-router-dom";
 import UserContext from "../../contexts/UserContext";
+import cartService from "../../services/cartService";
+import CartContext from "../../contexts/CartContext";
 
 export default function Header() {
+  const cartContext = useContext(CartContext);
   const userContext = useContext(UserContext);
+
+  useEffect(() => {
+    async function tryGetCart() {
+      const data = await cartService.getCart();
+
+      cartContext.setCart(data);
+    }
+
+    tryGetCart();
+  }, []);
 
   return (
     <header className="flex justify-between p-3 bg-dark text-bright">
       <h1 className="text-2xl font-bold">Mediabasen</h1>
       <ul className="flex gap-5">
-        <li>
+        <li className="relative">
           <CartLogo className="w-8 -scale-x-100" />
+          {cartContext.cart && (
+            <div className="absolute bottom-2 -left-2 bg-accent w-6 h-6 flex flex-col justify-center align-middle rounded-full">
+              <p className="block font-bold text-center w-fit flex-grow-0 mx-auto flex-shrink-0">
+                {cartContext.cart.cartProducts.length}
+              </p>
+            </div>
+          )}
         </li>
         <li>
           <Link
