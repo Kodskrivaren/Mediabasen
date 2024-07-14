8627709fea1a2b1354147f92dae2ec433e543213
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-14 13:15:42 +0200

    models for orders

---
+++ Mediabasen.Models/Order/Order.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mediabasen.Models.Order
+{
+    public class Order
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        [NotMapped]
+        public ApplicationUser User { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public DateTime? OrderShipped { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+    }
+}
---
+++ Mediabasen.Models/Order/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mediabasen.Models.Order
+{
+    public class OrderItem
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        [NotMapped]
+        public Product.Product Product { get; set; }
+    }
+}
