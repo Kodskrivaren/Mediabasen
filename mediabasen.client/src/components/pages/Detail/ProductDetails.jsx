f6676776f7d9a077c281d72deccc6af3284bd4a7
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-14 19:29:21 +0200

    altered for displaying different product types

--- mediabasen.client/src/components/pages/Detail/ProductDetails.jsx
+++ mediabasen.client/src/components/pages/Detail/ProductDetails.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import NoImageIcon from "../../../assets/icons/no-image-outline.svg?react";
 import AddToCartBtn from "../../globals/AddToCartBtn";
 import typeHelper from "../../../utils/typeHelper";
+import MovieDetails from "./MovieDetails";
+import MusicDetails from "./MusicDetails";
+import ProductPrice from "./ProductPrice";
+import DiscountSticker from "./DiscountSticker";
 
 export default function ProductDetails({ product }) {
   function hasImages() {
@@ -17,26 +21,19 @@ export default function ProductDetails({ product }) {
         </span>
         <span className="inline text-gray-300">{product.format.name}</span>
       </div>
-      {hasImages() ? (
-        <img src={`${product.images[0].imageUrl}`} />
-      ) : (
-        <NoImageIcon />
-      )}
-      <span className="text-xl font-bold">{product.price} kr</span>
+      <div className="relative w-full">
+        {hasImages() ? (
+          <img className="w-full" src={`${product.images[0].imageUrl}`} />
+        ) : (
+          <NoImageIcon />
+        )}
+        {product.discountedPrice && <DiscountSticker {...{ product }} />}
+      </div>
+      <ProductPrice {...{ product }} />
       <h3 className="text-2xl font-bold">Beskrivning</h3>
       <p className="text-xl">{product.description}</p>
       <h3 className="text-2xl font-bold">Specifikationer</h3>
       <div className="flex flex-wrap gap-y-2">
-        <span className="w-1/2 text-gray-300">Regissör</span>
-        <span className="w-1/2">{product.director.fullname}</span>
-        <span className="w-1/2 text-gray-300">Skådespelare</span>
-        <ul className="w-1/2">
-          {product.actors.map((actor) => (
-            <li key={`actor-${actor.id}`}>
-              <span>{actor.fullname}</span>
-            </li>
-          ))}
-        </ul>
         <span className="w-1/2 text-gray-300">Format</span>
         <span className="w-1/2">{product.format.name}</span>
         <span className="w-1/2 text-gray-300">Genre</span>
@@ -47,8 +44,18 @@ export default function ProductDetails({ product }) {
             </li>
           ))}
         </ul>
+        {product.productType.name === typeHelper.productTypes.Movie && (
+          <MovieDetails {...{ product }} />
+        )}
+        {product.productType.name === typeHelper.productTypes.Music && (
+          <MusicDetails {...{ product }} />
+        )}
         <span className="w-1/2 text-gray-300">Utgivet</span>
-        <span className="w-1/2">2007</span>
+        <span className="w-1/2">
+          {product.releaseDate
+            ? new Date(product.releaseDate).toLocaleDateString()
+            : "Okänt"}
+        </span>
       </div>
       <AddToCartBtn {...{ product }} />
     </section>
