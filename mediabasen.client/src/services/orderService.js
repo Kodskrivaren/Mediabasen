7f3db787d3392d99bad53ba823332b7d9b6aa869
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-14 13:12:05 +0200

    service for orders

---
+++ mediabasen.client/src/services/orderService.js
@@ -0,0 +1,19 @@
+import fetchHelper from "../utils/fetchHelper";
+
+async function getOrders() {
+  const response = await fetchHelper("/Order/GetOrders");
+
+  if (response.status < 400) {
+    return await response.json();
+  }
+}
+
+async function placeOrder() {
+  const response = await fetchHelper("/Order/PlaceOrder");
+
+  if (response.status < 400) {
+    return await response.json();
+  }
+}
+
+export default { getOrders, placeOrder };
