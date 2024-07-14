f92c90e3ed1b28ea11b97a2d67c3f61dd0354833
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-14 15:40:19 +0200

    model for music products

---
+++ Mediabasen.Models/Product/Music/ProductMusic.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mediabasen.Models.Product.Music
+{
+    public class ProductMusic : Product
+    {
+        public int ArtistId { get; set; }
+        [ForeignKey("ArtistId")]
+        [NotMapped]
+        public Name Artist { get; set; }
+        public int LabelId { get; set; }
+        [ForeignKey("LabelId")]
+        [NotMapped]
+        public Name Label { get; set; }
+        public int PublisherId { get; set; }
+        [ForeignKey("PublisherId")]
+        [NotMapped]
+        public Name Publisher { get; set; }
+    }
+}
