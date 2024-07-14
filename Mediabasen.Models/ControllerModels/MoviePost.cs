84e203fc229294ad5a73136ac2f5294ab642d0b4
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-14 15:39:44 +0200

    added release date

--- Mediabasen.Models/ControllerModels/MoviePost.cs
+++ Mediabasen.Models/ControllerModels/MoviePost.cs
@@ -14,5 +14,6 @@ namespace Mediabasen.Models.ControllerModels
         public List<int>? ActorIds { get; set; }
         public List<int>? GenreIds { get; set; }
         public IEnumerable<IFormFile>? Images { get; set; }
+        public DateTime ReleaseDate { get; set; }
     }
 }
