1d0955cacde3dc15bcbf9b215848393f34ca3108
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-14 13:15:15 +0200

    broke out some logic to other components

--- mediabasen.client/src/components/pages/User/UserPage.jsx
+++ mediabasen.client/src/components/pages/User/UserPage.jsx
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import UserContext from "../../../contexts/UserContext";
-import Button from "../../globals/Button";
-import authService from "../../../services/authService";
+import { Routes, Route } from "react-router-dom";
+import UserIndex from "./UserIndex";
+import UserOrders from "./UserOrders";
 
 export default function UserPage() {
   const userContext = useContext(UserContext);
@@ -14,36 +15,12 @@ export default function UserPage() {
     }
   }, [userContext]);
 
-  async function onLogoutClick() {
-    const result = await authService.logout();
-
-    if (result) {
-      userContext.setUser(undefined);
-      navigate("/");
-    }
-  }
-
-  function onAdminClick() {
-    navigate("/admin");
-  }
-
-  function isAdmin() {
-    if (!userContext.user) return false;
-
-    if (!userContext.user.roles.includes("Admin")) return false;
-
-    return true;
-  }
-
   return (
     <section className="text-white p-3">
-      <h2 className="">Din profil</h2>
-      {isAdmin() && (
-        <Button className="w-fit" onClick={onAdminClick}>
-          Admin-vyn
-        </Button>
-      )}
-      <Button onClick={onLogoutClick}>Logga ut</Button>
+      <Routes>
+        <Route index element={<UserIndex />} />
+        <Route path="orders" element={<UserOrders />} />
+      </Routes>
     </section>
   );
 }
