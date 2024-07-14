90ad3d31888a163b42ab1d4d68c5e55b7c5e8fc9
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-14 15:41:53 +0200

    repository for product music

---
+++ Mediabasen.ApplicationDbContext/Repository/IRepository/IProductMusicRepository.cs
@@ -0,0 +1,9 @@
+﻿using Mediabasen.Models.Product.Music;
+
+namespace Mediabasen.DataAccess.Repository.IRepository
+{
+    public interface IProductMusicRepository : IRepository<ProductMusic>
+    {
+        public void Update(ProductMusic productMusic);
+    }
+}
---
+++ Mediabasen.ApplicationDbContext/Repository/ProductMusicRepository.cs
@@ -0,0 +1,20 @@
+﻿using Mediabasen.DataAccess.Data;
+using Mediabasen.DataAccess.Repository.IRepository;
+using Mediabasen.Models.Product.Music;
+
+namespace Mediabasen.DataAccess.Repository
+{
+    public class ProductMusicRepository : Repository<ProductMusic>, IProductMusicRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductMusicRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(ProductMusic product)
+        {
+            _db.ProductMusic.Update(product);
+        }
+    }
+}
