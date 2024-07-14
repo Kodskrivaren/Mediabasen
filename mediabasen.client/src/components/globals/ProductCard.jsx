5549c6beb0d03517820f161a532c6eb533f17d50
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-14 19:28:59 +0200

    renders out discount sticker and price

--- mediabasen.client/src/components/globals/ProductCard.jsx
+++ mediabasen.client/src/components/globals/ProductCard.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Image from "../../assets/icons/no-image-outline.svg?react";
 import { Link } from "react-router-dom";
 import AddToCartBtn from "./AddToCartBtn";
+import ProductPrice from "../pages/Detail/ProductPrice";
+import DiscountSticker from "../pages/Detail/DiscountSticker";
 
 export default function ProductCard({ product }) {
   const imageUrl =
@@ -9,16 +11,29 @@ export default function ProductCard({ product }) {
 
   return (
     <article className="bg-light text-white p-2 w-full rounded flex flex-col gap-1">
-      <Link to={`/detail/${product ? product.id : ""}`}>
+      <Link
+        className="relative h-52"
+        to={`/detail/${product ? product.id : ""}`}>
         {imageUrl ? (
-          <img src={imageUrl} className="w-full object-contain h-52" />
+          <img src={imageUrl} className="w-full object-contain h-full" />
         ) : (
           <Image className="h-52 w-full" />
+        )}
+        {product.discountedPrice && (
+          <DiscountSticker
+            {...{
+              product,
+              className: "w-16 h-16 top-1 right-1",
+              textSize: "2xl",
+            }}
+          />
         )}
       </Link>
       <span className="text-gray-300">{product.format.name}</span>
       <h3 className="font-bold">{product.name}</h3>
-      <p className="pr-2">{product.price} kr</p>
+      <p>
+        <ProductPrice {...{ product, className: "" }} />
+      </p>
       <AddToCartBtn {...{ product }} />
     </article>
   );
