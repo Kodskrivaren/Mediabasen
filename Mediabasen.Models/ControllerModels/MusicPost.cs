475dd64b69b5dbe885d4f2c3bc69cae536948bf5
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-14 15:39:55 +0200

    model for posting music

---
+++ Mediabasen.Models/ControllerModels/MusicPost.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Mediabasen.Models.ControllerModels
+{
+    public class MusicPost
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public int ArtistId { get; set; }
+        public int FormatId { get; set; }
+        public int LabelId { get; set; }
+        public int PublisherId { get; set; }
+        public List<int>? GenreIds { get; set; }
+        public IEnumerable<IFormFile>? Images { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
