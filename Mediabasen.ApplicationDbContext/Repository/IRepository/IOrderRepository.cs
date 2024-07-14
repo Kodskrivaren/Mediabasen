f93fbb5716aabdc59525ecc85ad40dc502f9e290
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-14 13:16:15 +0200

    repository for order

---
+++ Mediabasen.ApplicationDbContext/Repository/IRepository/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using Mediabasen.Models.Order;
+
+namespace Mediabasen.DataAccess.Repository.IRepository
+{
+    public interface IOrderRepository : IRepository<Order>
+    {
+        public void Update(Order orders);
+    }
+}
---
+++ Mediabasen.ApplicationDbContext/Repository/OrderRepository.cs
@@ -0,0 +1,20 @@
+﻿using Mediabasen.DataAccess.Data;
+using Mediabasen.DataAccess.Repository.IRepository;
+using Mediabasen.Models.Order;
+
+namespace Mediabasen.DataAccess.Repository
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        private ApplicationDbContext _db;
+        public OrderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Order order)
+        {
+            _db.Orders.Update(order);
+        }
+    }
+}
