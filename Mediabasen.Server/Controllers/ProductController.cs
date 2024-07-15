a2229021eef582e0788e7447357043acdd6a1d82
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-15 20:26:34 +0200

    refactored help functions to its own class

--- Mediabasen.Server/Controllers/ProductController.cs
+++ Mediabasen.Server/Controllers/ProductController.cs
@@ -1,7 +1,6 @@
 ﻿using Mediabasen.DataAccess.Repository.IRepository;
 using Mediabasen.Models.Product;
-using Mediabasen.Models.Product.Movie;
-using Mediabasen.Models.Product.Music;
+using Mediabasen.Server.Services;
 using Mediabasen.Utility.SD;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -14,12 +13,14 @@ namespace Mediabasen.Server.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly ProductService _productService;
 
         [ActivatorUtilitiesConstructor]
-        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment, ProductService productService)
         {
             _unitOfWork = unitOfWork;
             _webHostEnvironment = webHostEnvironment;
+            _productService = productService;
         }
 
         [HttpGet]
@@ -28,7 +29,7 @@ namespace Mediabasen.Server.Controllers
             List<Product> products = _unitOfWork.Product.GetAll().ToList();
             foreach (var product in products)
             {
-                SetBasicProperties(product, _unitOfWork.ProductType.GetFirstOrDefault(u => u.Id == product.ProductTypeId));
+                _productService.SetBasicProperties(product, _unitOfWork.ProductType.GetFirstOrDefault(u => u.Id == product.ProductTypeId));
             }
             JsonResult res = new JsonResult(new { products });
             return res;
@@ -46,9 +47,9 @@ namespace Mediabasen.Server.Controllers
             switch (product.ProductType.Name)
             {
                 case SD.Type_Movie:
-                    return new JsonResult(GetProductMovie(product));
+                    return new JsonResult(_productService.GetProductMovie(product));
                 case SD.Type_Music:
-                    return new JsonResult(GetProductMusic(product));
+                    return new JsonResult(_productService.GetProductMusic(product));
                 default:
                     return new JsonResult(product);
             }
@@ -89,67 +90,5 @@ namespace Mediabasen.Server.Controllers
 
             return Ok();
         }
-
-        #region HelpFunctions
-        private List<ProductImage> GetProductImages(Product product)
-        {
-            List<ProductImage> Images = _unitOfWork.ProductImage.GetAll(u => u.ProductId == product.Id).ToList();
-            foreach (var image in Images)
-            {
-                image.Product = null;
-            }
-
-            return Images;
-        }
-
-        private void SetBasicProperties(Product product, ProductType productType)
-        {
-            product.Images = GetProductImages(product);
-
-            product.ProductType = productType;
-
-            product.Format = _unitOfWork.Format.GetFirstOrDefault(u => u.Id == product.FormatId);
-
-            var productGenres = _unitOfWork.ProductGenre.GetAll(u => u.ProductId == product.Id).ToList();
-
-            product.Genres = _unitOfWork.Genre.GetAll().Where(genre => productGenres.Find(productGenre => productGenre.GenreId == genre.Id) != null).ToList();
-
-            if (product.Discount > 0)
-            {
-                product.DiscountedPrice = Math.Round(product.Price - ((product.Discount / 100) * product.Price));
-            }
-        }
-
-        private ProductMovie GetProductMovie(Product product)
-        {
-            var movie = _unitOfWork.ProductMovie.GetFirstOrDefault(u => u.Id == product.Id);
-
-            SetBasicProperties(movie, product.ProductType);
-
-            movie.Director = _unitOfWork.Name.GetFirstOrDefault(u => u.Id == movie.DirectorNameId);
-
-            var movieActorIds = _unitOfWork.MovieActor.GetAll(u => u.ProductMovieId == movie.Id).ToList();
-
-            movie.Actors = _unitOfWork.Name.GetAll().Where(name => movieActorIds.Find(movieActor => name.Id == movieActor.NameId) != null).ToList();
-
-            return movie;
-        }
-
-        private ProductMusic GetProductMusic(Product product)
-        {
-            var music = _unitOfWork.ProductMusic.GetFirstOrDefault(u => u.Id == product.Id);
-
-            SetBasicProperties(music, product.ProductType);
-
-            music.Artist = _unitOfWork.Name.GetFirstOrDefault(u => u.Id == music.ArtistId);
-
-            music.Label = _unitOfWork.Name.GetFirstOrDefault(u => u.Id == music.LabelId);
-
-            music.Publisher = _unitOfWork.Name.GetFirstOrDefault(u => u.Id == music.PublisherId);
-
-            return music;
-        }
-
-        #endregion
     }
 }
---
+++ Mediabasen.Server/Services/ProductService.cs
@@ -0,0 +1,81 @@
+﻿using Mediabasen.DataAccess.Repository.IRepository;
+using Mediabasen.Models.Product;
+using Mediabasen.Models.Product.Movie;
+using Mediabasen.Models.Product.Music;
+
+namespace Mediabasen.Server.Services
+{
+    public class ProductService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        private List<ProductImage> GetProductImages(Product product)
+        {
+            List<ProductImage> Images = _unitOfWork.ProductImage.GetAll(u => u.ProductId == product.Id).ToList();
+            foreach (var image in Images)
+            {
+                image.Product = null;
+            }
+
+            return Images;
+        }
+
+        public void SetBasicProperties(Product product, ProductType productType)
+        {
+            product.Images = GetProductImages(product);
+
+            product.ProductType = productType;
+
+            product.Format = _unitOfWork.Format.GetFirstOrDefault(u => u.Id == product.FormatId);
+
+            var productGenres = _unitOfWork.ProductGenre.GetAll(u => u.ProductId == product.Id).ToList();
+
+            product.Genres = _unitOfWork.Genre.GetAll().Where(genre => productGenres.Find(productGenre => productGenre.GenreId == genre.Id) != null).ToList();
+
+            if (product.Discount > 0)
+            {
+                product.DiscountedPrice = GetCalculatedDiscountedPrice(product);
+            }
+        }
+
+        public decimal GetCalculatedDiscountedPrice(Product product)
+        {
+            return Math.Round(product.Price - ((product.Discount / 100) * product.Price));
+        }
+
+        public ProductMovie GetProductMovie(Product product)
+        {
+            var movie = _unitOfWork.ProductMovie.GetFirstOrDefault(u => u.Id == product.Id);
+
+            SetBasicProperties(movie, product.ProductType);
+
+            movie.Director = _unitOfWork.Name.GetFirstOrDefault(u => u.Id == movie.DirectorNameId);
+
+            var movieActorIds = _unitOfWork.MovieActor.GetAll(u => u.ProductMovieId == movie.Id).ToList();
+
+            movie.Actors = _unitOfWork.Name.GetAll().Where(name => movieActorIds.Find(movieActor => name.Id == movieActor.NameId) != null).ToList();
+
+            return movie;
+        }
+
+        public ProductMusic GetProductMusic(Product product)
+        {
+            var music = _unitOfWork.ProductMusic.GetFirstOrDefault(u => u.Id == product.Id);
+
+            SetBasicProperties(music, product.ProductType);
+
+            music.Artist = _unitOfWork.Name.GetFirstOrDefault(u => u.Id == music.ArtistId);
+
+            music.Label = _unitOfWork.Name.GetFirstOrDefault(u => u.Id == music.LabelId);
+
+            music.Publisher = _unitOfWork.Name.GetFirstOrDefault(u => u.Id == music.PublisherId);
+
+            return music;
+        }
+    }
+}
