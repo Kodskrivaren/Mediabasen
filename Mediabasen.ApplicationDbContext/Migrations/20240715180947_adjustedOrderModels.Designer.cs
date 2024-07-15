346fd634e59f989ab59efe73f3afb2b94d6fb6de
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-15 20:25:44 +0200

    db migration for model change

--- Mediabasen.ApplicationDbContext/Migrations/20240715180947_adjustedOrderModels.Designer.cs
+++ Mediabasen.ApplicationDbContext/Migrations/20240715180947_adjustedOrderModels.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Mediabasen.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Mediabasen.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240715180947_adjustedOrderModels")]
+    partial class adjustedOrderModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Mediabasen.ApplicationDbContext/Migrations/20240715180947_adjustedOrderModels.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mediabasen.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class adjustedOrderModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discount",
+                table: "OrderItem");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalPrice",
+                table: "Orders",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalPrice",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Discount",
+                table: "OrderItem",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
