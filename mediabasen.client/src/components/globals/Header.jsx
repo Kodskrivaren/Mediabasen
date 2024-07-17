1e6a98f73fa55eee3008ddf790c254fc8c14f9a6
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-17 20:11:10 +0200

    styled for tablet and desktop

--- mediabasen.client/src/components/globals/Header.jsx
+++ mediabasen.client/src/components/globals/Header.jsx
@@ -21,36 +21,38 @@ export default function Header() {
   }, []);
 
   return (
-    <header className="flex justify-between p-3 bg-dark text-bright">
-      <Link to={"/"}>
-        <h1 className="text-2xl font-bold">Mediabasen</h1>
-      </Link>
-      <ul className="flex gap-5">
-        <li className="relative">
-          <Link to={"/cart"}>
-            <CartLogo className="w-8 -scale-x-100" />
-            {cartContext.cart && (
-              <div className="absolute bottom-2 -left-2 bg-accent w-6 h-6 flex flex-col justify-center align-middle rounded-full">
-                <p className="block font-bold text-center w-fit flex-grow-0 mx-auto flex-shrink-0">
-                  {cartContext.cart.cartProducts.length}
-                </p>
-              </div>
-            )}
-          </Link>
-        </li>
-        <li>
-          <Link
-            to={userContext.user ? "/user" : "/login"}
-            className="text-center">
-            <UserLogo className="w-8 mx-auto" />
-            <p className="text-sm">
-              {userContext.user
-                ? userContext.user.name.split(" ")[0]
-                : "Logga in"}
-            </p>
-          </Link>
-        </li>
-      </ul>
+    <header className="bg-dark">
+      <section className="flex justify-between p-3 text-bright mx-auto max-w-7xl">
+        <Link to={"/"}>
+          <h1 className="text-2xl font-bold">Mediabasen</h1>
+        </Link>
+        <ul className="flex gap-5">
+          <li className="relative">
+            <Link to={"/cart"}>
+              <CartLogo className="w-8 -scale-x-100" />
+              {cartContext.cart && (
+                <div className="absolute bottom-2 -left-2 bg-accent w-6 h-6 flex flex-col justify-center align-middle rounded-full">
+                  <p className="block font-bold text-center w-fit flex-grow-0 mx-auto flex-shrink-0">
+                    {cartContext.cart.cartProducts.length}
+                  </p>
+                </div>
+              )}
+            </Link>
+          </li>
+          <li>
+            <Link
+              to={userContext.user ? "/user" : "/login"}
+              className="text-center">
+              <UserLogo className="w-8 mx-auto" />
+              <p className="text-sm">
+                {userContext.user
+                  ? userContext.user.name.split(" ")[0]
+                  : "Logga in"}
+              </p>
+            </Link>
+          </li>
+        </ul>
+      </section>
     </header>
   );
 }
