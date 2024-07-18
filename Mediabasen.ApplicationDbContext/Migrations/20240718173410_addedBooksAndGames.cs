e94a8164891696150ba4c391d505ec25e96a7148
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-18 19:36:53 +0200

    migration for books and games

---
+++ Mediabasen.ApplicationDbContext/Migrations/20240718173410_addedBooksAndGames.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mediabasen.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedBooksAndGames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "Products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DeveloperId",
+                table: "Products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductBook_PublisherId",
+                table: "Products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductGame_PublisherId",
+                table: "Products",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "DeveloperId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ProductBook_PublisherId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ProductGame_PublisherId",
+                table: "Products");
+        }
+    }
+}
