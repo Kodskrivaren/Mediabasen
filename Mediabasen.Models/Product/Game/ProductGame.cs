cbb0d6658769505fd8a86556accfca3e576b990b
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-18 19:32:17 +0200

    model for games

---
+++ Mediabasen.Models/Product/Game/ProductGame.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mediabasen.Models.Product.Game
+{
+    public class ProductGame : Product
+    {
+        public int DeveloperId { get; set; }
+        [NotMapped]
+        [ForeignKey("DeveloperId")]
+        public Name Developer { get; set; }
+        public int PublisherId { get; set; }
+        [NotMapped]
+        [ForeignKey("PublisherId")]
+        public Name Publisher { get; set; }
+    }
+}
