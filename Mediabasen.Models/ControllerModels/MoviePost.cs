cc282904b9415d1857fed99cc9249aaeda905dfa
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-18 19:33:20 +0200

    adjusted for product post class

--- Mediabasen.Models/ControllerModels/MoviePost.cs
+++ Mediabasen.Models/ControllerModels/MoviePost.cs
@@ -1,19 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace Mediabasen.Models.ControllerModels
+﻿namespace Mediabasen.Models.ControllerModels
 {
-    public class MoviePost
+    public class MoviePost : ProductPost
     {
-        public int? Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public decimal Discount { get; set; }
         public int DirectorId { get; set; }
-        public int FormatId { get; set; }
         public List<int>? ActorIds { get; set; }
-        public List<int>? GenreIds { get; set; }
-        public IEnumerable<IFormFile>? Images { get; set; }
-        public DateTime ReleaseDate { get; set; }
     }
 }
