147cc1ea7734647f737e23a0db36a991bc87fd2d
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-18 19:33:33 +0200

    post class for book

---
+++ Mediabasen.Models/ControllerModels/BookPost.cs
@@ -0,0 +1,8 @@
+﻿namespace Mediabasen.Models.ControllerModels
+{
+    public class BookPost : ProductPost
+    {
+        public int AuthorId { get; set; }
+        public int PublisherId { get; set; }
+    }
+}
