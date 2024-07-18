a66eb0ead1706de931eafbfa91c7ce1c8624ef02
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-18 20:08:16 +0200

    pages for adding books and games

---
+++ mediabasen.client/src/components/pages/Admin/AddBook.jsx
@@ -0,0 +1,166 @@
+import React, { useState } from "react";
+import Input from "../../globals/Input";
+import useSearchHook from "../../../hooks/useSearchHook";
+import AddNameModal from "./AddNameModal";
+import NameList from "./NameList";
+import Button from "../../globals/Button";
+import bookService from "../../../services/bookService";
+import Modal from "../../globals/Modal";
+import nameService from "../../../services/nameService";
+import AddBaseForm from "./AddBaseForm";
+
+export default function AddBook() {
+  const [authorNotFound, setAuthorNotFound] = useState(undefined);
+  const [authorId, setAuthorId] = useState(undefined);
+
+  const [publisherNotFound, setPublisherNotFound] = useState(undefined);
+  const [publisherId, setPublisherId] = useState(undefined);
+
+  const [selectedFormat, setSelectedFormat] = useState(undefined);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const [imageFiles, setImageFiles] = useState([]);
+  const [result, setResult] = useState();
+
+  const {
+    search: authorNameSearch,
+    setSearch: setAuthorNameSearch,
+    setPreventSearch: preventAuthorSearch,
+    searchResult: authorNameSearchResult,
+    setSearchResult: setAuthorNameSearchResult,
+  } = useSearchHook({
+    setNotFound: setAuthorNotFound,
+    searchFunction: nameService.findNames,
+  });
+
+  const {
+    search: publisherNameSearch,
+    setSearch: setPublisherNameSearch,
+    setPreventSearch: preventPublisherSearch,
+    searchResult: publisherNameSearchResult,
+    setSearchResult: setPublisherNameSearchResult,
+  } = useSearchHook({
+    setNotFound: setPublisherNotFound,
+    searchFunction: nameService.findNames,
+  });
+
+  async function postBook(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const objectValues = Object.fromEntries(formData);
+
+    objectValues.Id = 0;
+    objectValues.Images = imageFiles;
+    objectValues.GenreIds = selectedGenres;
+    objectValues.FormatId = selectedFormat.id;
+    objectValues.PublisherId = publisherId;
+    objectValues.AuthorId = authorId;
+
+    const result = await bookService.addBook(objectValues);
+
+    if (result) {
+      setResult(result);
+    }
+  }
+
+  function onNameClick(
+    name,
+    setNameState,
+    setNameId,
+    preventSearch,
+    setNameSearchResult
+  ) {
+    setNameState(name.fullname);
+    setNameId(name.id);
+    preventSearch(true);
+    setNameSearchResult([]);
+  }
+
+  return (
+    <>
+      <h2 className="text-white">Lägg till bok</h2>
+      <form className="flex flex-col gap-3 max-w-96" onSubmit={postBook}>
+        <AddBaseForm
+          {...{
+            selectedGenres,
+            setSelectedGenres,
+            imageFiles,
+            setImageFiles,
+            selectedFormat,
+            setSelectedFormat,
+          }}
+        />
+        <label className="text-white">Författare</label>
+        <Input
+          placeholder={"Sök på namn..."}
+          state={authorNameSearch}
+          setState={setAuthorNameSearch}
+        />
+        {authorNameSearchResult.length !== 0 && (
+          <NameList
+            nameSearchResult={authorNameSearchResult}
+            onNameClick={(name) =>
+              onNameClick(
+                name,
+                setAuthorNameSearch,
+                setAuthorId,
+                preventAuthorSearch,
+                setAuthorNameSearchResult
+              )
+            }
+            listKeyPrefix="artist"
+          />
+        )}
+        <label className="text-white">Utgivare</label>
+        <Input
+          placeholder={"Sök på namn..."}
+          state={publisherNameSearch}
+          setState={setPublisherNameSearch}
+        />
+        {publisherNameSearchResult.length !== 0 && (
+          <NameList
+            nameSearchResult={publisherNameSearchResult}
+            onNameClick={(name) =>
+              onNameClick(
+                name,
+                setPublisherNameSearch,
+                setPublisherId,
+                preventPublisherSearch,
+                setPublisherNameSearchResult
+              )
+            }
+            listKeyPrefix="publisher"
+          />
+        )}
+        <Button classNameColor="bg-accent" className="w-fit" type="submit">
+          Lägg till boken
+        </Button>
+      </form>
+      {publisherNameSearchResult.length === 0 && publisherNotFound && (
+        <AddNameModal
+          {...{
+            setNameSearch: setPublisherNameSearch,
+            preventNameSearch: preventPublisherSearch,
+            nameNotFound: publisherNotFound,
+            setNameNotFound: setPublisherNotFound,
+          }}
+        />
+      )}
+      {authorNameSearchResult.length === 0 && authorNotFound && (
+        <AddNameModal
+          {...{
+            setNameSearch: setAuthorNameSearch,
+            preventNameSearch: preventAuthorSearch,
+            nameNotFound: authorNotFound,
+            setNameNotFound: setAuthorNotFound,
+          }}
+        />
+      )}
+      {result && (
+        <Modal setClose={setResult} closeValue={undefined}>
+          Boken har lagts till!
+        </Modal>
+      )}
+    </>
+  );
+}
---
+++ mediabasen.client/src/components/pages/Admin/AddGame.jsx
@@ -0,0 +1,166 @@
+import React, { useState } from "react";
+import Input from "../../globals/Input";
+import useSearchHook from "../../../hooks/useSearchHook";
+import AddNameModal from "./AddNameModal";
+import NameList from "./NameList";
+import Button from "../../globals/Button";
+import gameService from "../../../services/gameService";
+import Modal from "../../globals/Modal";
+import nameService from "../../../services/nameService";
+import AddBaseForm from "./AddBaseForm";
+
+export default function AddGame() {
+  const [developerNotFound, setDeveloperNotFound] = useState(undefined);
+  const [developerId, setDeveloperId] = useState(undefined);
+
+  const [publisherNotFound, setPublisherNotFound] = useState(undefined);
+  const [publisherId, setPublisherId] = useState(undefined);
+
+  const [selectedFormat, setSelectedFormat] = useState(undefined);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const [imageFiles, setImageFiles] = useState([]);
+  const [result, setResult] = useState();
+
+  const {
+    search: developerNameSearch,
+    setSearch: setDeveloperNameSearch,
+    setPreventSearch: preventDeveloperSearch,
+    searchResult: developerNameSearchResult,
+    setSearchResult: setDeveloperNameSearchResult,
+  } = useSearchHook({
+    setNotFound: setDeveloperNotFound,
+    searchFunction: nameService.findNames,
+  });
+
+  const {
+    search: publisherNameSearch,
+    setSearch: setPublisherNameSearch,
+    setPreventSearch: preventPublisherSearch,
+    searchResult: publisherNameSearchResult,
+    setSearchResult: setPublisherNameSearchResult,
+  } = useSearchHook({
+    setNotFound: setPublisherNotFound,
+    searchFunction: nameService.findNames,
+  });
+
+  async function postGame(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const objectValues = Object.fromEntries(formData);
+
+    objectValues.Id = 0;
+    objectValues.Images = imageFiles;
+    objectValues.GenreIds = selectedGenres;
+    objectValues.FormatId = selectedFormat.id;
+    objectValues.PublisherId = publisherId;
+    objectValues.DeveloperId = developerId;
+
+    const result = await gameService.addGame(objectValues);
+
+    if (result) {
+      setResult(result);
+    }
+  }
+
+  function onNameClick(
+    name,
+    setNameState,
+    setNameId,
+    preventSearch,
+    setNameSearchResult
+  ) {
+    setNameState(name.fullname);
+    setNameId(name.id);
+    preventSearch(true);
+    setNameSearchResult([]);
+  }
+
+  return (
+    <>
+      <h2 className="text-white">Lägg till spel</h2>
+      <form className="flex flex-col gap-3 max-w-96" onSubmit={postGame}>
+        <AddBaseForm
+          {...{
+            selectedGenres,
+            setSelectedGenres,
+            imageFiles,
+            setImageFiles,
+            selectedFormat,
+            setSelectedFormat,
+          }}
+        />
+        <label className="text-white">Utvecklare</label>
+        <Input
+          placeholder={"Sök på namn..."}
+          state={developerNameSearch}
+          setState={setDeveloperNameSearch}
+        />
+        {developerNameSearchResult.length !== 0 && (
+          <NameList
+            nameSearchResult={developerNameSearchResult}
+            onNameClick={(name) =>
+              onNameClick(
+                name,
+                setDeveloperNameSearch,
+                setDeveloperId,
+                preventDeveloperSearch,
+                setDeveloperNameSearchResult
+              )
+            }
+            listKeyPrefix="artist"
+          />
+        )}
+        <label className="text-white">Utgivare</label>
+        <Input
+          placeholder={"Sök på namn..."}
+          state={publisherNameSearch}
+          setState={setPublisherNameSearch}
+        />
+        {publisherNameSearchResult.length !== 0 && (
+          <NameList
+            nameSearchResult={publisherNameSearchResult}
+            onNameClick={(name) =>
+              onNameClick(
+                name,
+                setPublisherNameSearch,
+                setPublisherId,
+                preventPublisherSearch,
+                setPublisherNameSearchResult
+              )
+            }
+            listKeyPrefix="publisher"
+          />
+        )}
+        <Button classNameColor="bg-accent" className="w-fit" type="submit">
+          Lägg till spelet
+        </Button>
+      </form>
+      {publisherNameSearchResult.length === 0 && publisherNotFound && (
+        <AddNameModal
+          {...{
+            setNameSearch: setPublisherNameSearch,
+            preventNameSearch: preventPublisherSearch,
+            nameNotFound: publisherNotFound,
+            setNameNotFound: setPublisherNotFound,
+          }}
+        />
+      )}
+      {developerNameSearchResult.length === 0 && developerNotFound && (
+        <AddNameModal
+          {...{
+            setNameSearch: setDeveloperNameSearch,
+            preventNameSearch: preventDeveloperSearch,
+            nameNotFound: developerNotFound,
+            setNameNotFound: setDeveloperNotFound,
+          }}
+        />
+      )}
+      {result && (
+        <Modal setClose={setResult} closeValue={undefined}>
+          Spelet har lagts till!
+        </Modal>
+      )}
+    </>
+  );
+}
