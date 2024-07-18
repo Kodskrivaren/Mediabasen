a90cb4188ac6a9e04f0aeeadb87612b1a0804afd
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-18 19:32:56 +0200

    generic class for product post

---
+++ Mediabasen.Models/ControllerModels/ProductPost.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Mediabasen.Models.ControllerModels
+{
+    public class ProductPost
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public int FormatId { get; set; }
+        public List<int>? GenreIds { get; set; }
+        public IEnumerable<IFormFile>? Images { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
