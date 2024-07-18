31360d416a0e967367af702d2528154b3b3ebd73
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-18 19:32:09 +0200

    model for books

---
+++ Mediabasen.Models/Product/Book/ProductBook.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mediabasen.Models.Product.Book
+{
+    public class ProductBook : Product
+    {
+        public int AuthorId { get; set; }
+        [ForeignKey("AuthorId")]
+        [NotMapped]
+        public Name Author { get; set; }
+        public int PublisherId { get; set; }
+        [ForeignKey("PublisherId")]
+        [NotMapped]
+        public Name Publisher { get; set; }
+    }
+}
