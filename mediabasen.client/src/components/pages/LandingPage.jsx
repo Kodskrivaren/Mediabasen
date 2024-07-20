6d7a027e7435341a5604495f69d1e0da20c96cb7
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-20 09:31:27 +0200

    added product search

--- mediabasen.client/src/components/pages/LandingPage.jsx
+++ mediabasen.client/src/components/pages/LandingPage.jsx
@@ -1,10 +1,12 @@
 import CategoryPicker from "../globals/CategoryPicker";
+import ProductSearch from "../globals/ProductSearch";
 import NewProducts from "./Landing/NewProducts";
 
 export default function LandingPage() {
   return (
     <>
       <CategoryPicker />
+      <ProductSearch />
       <NewProducts />
     </>
   );
