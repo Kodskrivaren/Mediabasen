211cde6d00ffb5512a1e6e9172d3816058294b95
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-20 20:58:12 +0200

    component for editing a game

---
+++ mediabasen.client/src/components/pages/Admin/Edit/EditGame.jsx
@@ -0,0 +1,210 @@
+import React, { useState, useEffect } from "react";
+import gameService from "../../../../services/gameService";
+import EditBaseForm from "./EditBaseForm";
+import Button from "../../../globals/Button";
+import useBasicEditPropsHook from "../../../../hooks/useBasicEditPropsHook";
+import nameService from "../../../../services/nameService";
+import useSearchHook from "../../../../hooks/useSearchHook";
+import Input from "../../../globals/Input";
+import NameList from "../NameList";
+import Modal from "../../../globals/Modal";
+
+export default function EditGame({ product }) {
+  const [developerNotFound, setDeveloperNotFound] = useState(undefined);
+  const [publisherNotFound, setPublisherNotFound] = useState(undefined);
+  const [result, setResult] = useState();
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [discount, setDiscount] = useState(0);
+  const [releaseDate, setReleaseDate] = useState("");
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const [selectedFormat, setSelectedFormat] = useState();
+  const [imageFiles, setImageFiles] = useState([]);
+  const [developerId, setDeveloperId] = useState(0);
+  const [publisherId, setPublisherId] = useState(0);
+
+  const {
+    search: developerNameSearch,
+    setSearch: setDeveloperNameSearch,
+    setPreventSearch: preventDeveloperSearch,
+    searchResult: developerNameSearchResult,
+    setSearchResult: setDeveloperNameSearchResult,
+  } = useSearchHook({
+    setNotFound: setDeveloperNotFound,
+    searchFunction: nameService.findNames,
+  });
+
+  const {
+    search: publisherNameSearch,
+    setSearch: setPublisherNameSearch,
+    setPreventSearch: preventPublisherSearch,
+    searchResult: publisherNameSearchResult,
+    setSearchResult: setPublisherNameSearchResult,
+  } = useSearchHook({
+    setNotFound: setPublisherNotFound,
+    searchFunction: nameService.findNames,
+  });
+
+  async function onSaveChangesClick() {
+    const updatedProduct = { Id: product.id };
+
+    updatedProduct.Name = name;
+    updatedProduct.Description = description;
+    updatedProduct.Price = price;
+    updatedProduct.Discount = discount;
+    updatedProduct.ReleaseDate = releaseDate;
+    updatedProduct.Images = imageFiles;
+    updatedProduct.FormatId = selectedFormat.id;
+    updatedProduct.GenreIds = selectedGenres;
+
+    updatedProduct.DeveloperId = developerId;
+    updatedProduct.PublisherId = publisherId;
+
+    const data = await gameService.updateGame(updatedProduct);
+
+    setResult(data);
+  }
+
+  useBasicEditPropsHook({
+    product,
+    setImageFiles,
+    setSelectedGenres,
+    setName,
+    setDescription,
+    setPrice,
+    setDiscount,
+    setReleaseDate,
+    setSelectedFormat,
+  });
+
+  useEffect(() => {
+    if (!product) return;
+    setDeveloperId(product.developerId);
+    setDeveloperNameSearch(product.developer.fullname);
+    preventDeveloperSearch(true);
+
+    setPublisherId(product.publisherId);
+    setPublisherNameSearch(product.publisher.fullname);
+    preventPublisherSearch(true);
+  }, [product]);
+
+  function onNameClick(
+    name,
+    setNameState,
+    setNameId,
+    preventSearch,
+    setNameSearchResult
+  ) {
+    setNameState(name.fullname);
+    setNameId(name.id);
+    preventSearch(true);
+    setNameSearchResult([]);
+  }
+
+  return (
+    <>
+      <h2 className="text-white">Redigera produkt - Spel</h2>
+      {product && (
+        <>
+          <EditBaseForm
+            {...{
+              name,
+              setName,
+              description,
+              setDescription,
+              price,
+              setPrice,
+              discount,
+              setDiscount,
+              releaseDate,
+              setReleaseDate,
+              selectedGenres,
+              setSelectedGenres,
+              imageFiles,
+              setImageFiles,
+              selectedFormat,
+              setSelectedFormat,
+            }}
+          />
+          <label className="text-white">Utvecklare</label>
+          <Input
+            placeholder={"Sök på namn..."}
+            state={developerNameSearch}
+            setState={setDeveloperNameSearch}
+          />
+          {developerNameSearchResult.length !== 0 && (
+            <NameList
+              nameSearchResult={developerNameSearchResult}
+              onNameClick={(name) =>
+                onNameClick(
+                  name,
+                  setDeveloperNameSearch,
+                  setDeveloperId,
+                  preventDeveloperSearch,
+                  setDeveloperNameSearchResult
+                )
+              }
+              listKeyPrefix="artist"
+            />
+          )}
+          <label className="text-white">Utgivare</label>
+          <Input
+            placeholder={"Sök på namn..."}
+            state={publisherNameSearch}
+            setState={setPublisherNameSearch}
+          />
+          {publisherNameSearchResult.length !== 0 && (
+            <NameList
+              nameSearchResult={publisherNameSearchResult}
+              onNameClick={(name) =>
+                onNameClick(
+                  name,
+                  setPublisherNameSearch,
+                  setPublisherId,
+                  preventPublisherSearch,
+                  setPublisherNameSearchResult
+                )
+              }
+              listKeyPrefix="artist"
+            />
+          )}
+          <Button
+            classNameColor="bg-accent"
+            className="w-fit"
+            onClick={onSaveChangesClick}>
+            Spara ändringar
+          </Button>
+        </>
+      )}
+      {developerNameSearchResult.length === 0 && developerNotFound && (
+        <AddNameModal
+          {...{
+            setNameSearch: setDeveloperNameSearch,
+            preventNameSearch: preventDeveloperSearch,
+            nameNotFound: developerNotFound,
+            setNameNotFound: setDeveloperNotFound,
+            setNameId: setDeveloperId,
+          }}
+        />
+      )}
+      {publisherNameSearchResult.length === 0 && publisherNotFound && (
+        <AddNameModal
+          {...{
+            setNameSearch: setPublisherNameSearch,
+            preventNameSearch: preventPublisherSearch,
+            nameNotFound: publisherNotFound,
+            setNameNotFound: setPublisherNotFound,
+            setNameId: setPublisherId,
+          }}
+        />
+      )}
+      {result && (
+        <Modal setClose={setResult} closeValue={undefined}>
+          Ändringarna har sparats!
+        </Modal>
+      )}
+    </>
+  );
+}
