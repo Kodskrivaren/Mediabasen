3558a5b96b4d7105973f0be6916f6793be3a8fac
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-20 20:56:29 +0200

    added props for optional state

--- mediabasen.client/src/components/globals/Textarea.jsx
+++ mediabasen.client/src/components/globals/Textarea.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-export default function Textarea({ classNames, name, placeholder }) {
+export default function Textarea({
+  classNames,
+  name,
+  placeholder,
+  state,
+  setState,
+}) {
   return (
     <textarea
       className={`p-3 outline-none rounded${
@@ -8,6 +14,12 @@ export default function Textarea({ classNames, name, placeholder }) {
       }`}
       name={name}
       placeholder={placeholder}
+      value={state !== undefined ? state : undefined}
+      onChange={(e) => {
+        if (setState) {
+          setState(e.target.value);
+        }
+      }}
     />
   );
 }
