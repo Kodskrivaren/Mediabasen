18501c45834f68a18fc5e80c913c7e7a1eb1db34
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-21 10:55:55 +0200

    refactored for products list

--- mediabasen.client/src/components/pages/LandingPage.jsx
+++ mediabasen.client/src/components/pages/LandingPage.jsx
@@ -1,13 +1,26 @@
 import CategoryPicker from "../globals/CategoryPicker";
 import ProductSearch from "../globals/ProductSearch";
-import NewProducts from "./Landing/NewProducts";
+import ProductsList from "../globals/ProductsList";
+import productService from "../../services/productService";
 
 export default function LandingPage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const result = await productFetch();
+
+      setProducts(result.products);
+    }
+
+    fetchProducts();
+  }, [setProducts]);
+
   return (
     <>
       <CategoryPicker />
       <ProductSearch />
-      <NewProducts />
+      <ProductsList title={"Nya Produkter"} products={products} />
     </>
   );
 }
