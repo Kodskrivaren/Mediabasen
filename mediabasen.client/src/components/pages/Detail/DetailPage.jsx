dae1caeffe0dfabf09ff1d20c29b438cdac0a685
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-21 10:56:07 +0200

    detail page shows similar products

--- mediabasen.client/src/components/pages/Detail/DetailPage.jsx
+++ mediabasen.client/src/components/pages/Detail/DetailPage.jsx
@@ -3,8 +3,10 @@ import { useParams } from "react-router-dom";
 import productService from "../../../services/productService";
 import LoadSpinner from "../../globals/LoadSpinner";
 import ProductDetails from "./ProductDetails";
+import ProductsList from "../../globals/ProductsList";
 
 export default function DetailPage() {
+  const [similarProducts, setSimilarProducts] = useState([]);
   const [product, setProduct] = useState();
   const params = useParams();
 
@@ -14,13 +16,20 @@ export default function DetailPage() {
 
       setProduct(data);
     }
+    async function fetchProducts() {
+      const result = await productService.getSimilarProducts(params["*"]);
 
+      setSimilarProducts(result.products);
+    }
+
+    fetchProducts();
     getProduct();
-  }, []);
+  }, [params]);
 
   return product ? (
     <>
       <ProductDetails product={product} />
+      <ProductsList title={"Du kanske gillar"} products={similarProducts} />
     </>
   ) : (
     <div className="flex p-3 justify-center">
