0c8f8a7e00fe040e6a633784e2cc77196958dedd
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-29 11:22:24 +0200

    implemented method for search page with pagination

--- Mediabasen.ApplicationDbContext/Repository/IRepository/IProductRepository.cs
+++ Mediabasen.ApplicationDbContext/Repository/IRepository/IProductRepository.cs
@@ -7,5 +7,6 @@ namespace Mediabasen.DataAccess.Repository.IRepository
         public void Update(Product product);
         public IEnumerable<Product> GetNewestProducts();
         public IEnumerable<Product> SearchProducts(string query);
+        public IEnumerable<Product> FullSearchProducts(string? query, int[]? filter, int? page);
     }
 }
--- Mediabasen.ApplicationDbContext/Repository/ProductRepository.cs
+++ Mediabasen.ApplicationDbContext/Repository/ProductRepository.cs
@@ -24,7 +24,40 @@ namespace Mediabasen.DataAccess.Repository
 
         public IEnumerable<Product> SearchProducts(string query)
         {
-            return _db.Products.AsEnumerable().Where((u, index) => index < 10 && u.Name.ToLower().StartsWith(query.ToLower()));
+            return _db.Products
+                .AsEnumerable()
+                .Where((u) => u.Name.ToLower().Contains(query.ToLower()))
+                .Where((u, index) => index < 10);
+        }
+
+        public IEnumerable<Product> FullSearchProducts(string? query, int[]? filter, int? page)
+        {
+            int itemsPerPage = 5;
+
+            return _db.Products
+                        .ToList()
+                        .Where((u) => MatchesFilter(filter, u) && MatchesQuery(query, u))
+                        .Where((u, index) => IsInPage(page, index, itemsPerPage)
+                        );
+        }
+
+        private bool MatchesFilter(int[]? filter, Product u)
+        {
+            return filter != null && filter.Length != 0 ? filter.Contains(u.ProductTypeId) : true;
+        }
+
+        private bool MatchesQuery(string? query, Product u)
+        {
+            return query != null && query != "" ?
+                    u.Name.Trim().ToLower().Contains(query.ToLower()) :
+                    true;
+        }
+
+        private bool IsInPage(int? page, int index, int itemsPerPage)
+        {
+            return page != null && page > 1 ?
+                            index >= ((page - 1) * itemsPerPage) && index < page * itemsPerPage :
+                            index < itemsPerPage;
         }
     }
 }
