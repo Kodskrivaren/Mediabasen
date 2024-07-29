1b0383688b74527e02e2dd138356977881c69f20
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-29 17:16:00 +0200

    added navigation to search page and props

--- mediabasen.client/src/components/pages/LandingPage.jsx
+++ mediabasen.client/src/components/pages/LandingPage.jsx
@@ -4,13 +4,22 @@ import ProductSearch from "../globals/ProductSearch";
 import ProductsList from "../globals/ProductsList";
 import productService from "../../services/productService";
 import useFastSearchHook from "../../hooks/useFastSearchHook";
+import { useNavigate } from "react-router-dom";
+import searchHelper from "../../utils/searchHelper";
 
 export default function LandingPage() {
   const [searchQuery, setSearchQuery] = useState("");
   const [result, setResult] = useState(undefined);
   const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
 
   useFastSearchHook(searchQuery, setResult);
+
+  function onInputKeyDown(e) {
+    if (e.code.includes("Enter")) {
+      navigate(`/search?${searchHelper.searchQueries.query}=${searchQuery}`);
+    }
+  }
 
   useEffect(() => {
     async function fetchProducts() {
@@ -25,7 +34,14 @@ export default function LandingPage() {
   return (
     <>
       <CategoryPicker />
-      <ProductSearch {...{ searchQuery, setSearchQuery, result }} />
+      <ProductSearch
+        {...{
+          searchQuery,
+          setSearchQuery,
+          result,
+          inputOnKeyDown: onInputKeyDown,
+        }}
+      />
       <ProductsList title={"Nya Produkter"} products={products} />
     </>
   );
