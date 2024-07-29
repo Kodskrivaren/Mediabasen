10680306103777661b63a73edffa35a60a00b4b9
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-29 17:03:16 +0200

    optimized code to reduce client side filtering

--- Mediabasen.ApplicationDbContext/Repository/ProductRepository.cs
+++ Mediabasen.ApplicationDbContext/Repository/ProductRepository.cs
@@ -19,45 +19,40 @@ namespace Mediabasen.DataAccess.Repository
 
         public IEnumerable<Product> GetNewestProducts()
         {
-            return _db.Products.AsEnumerable().OrderByDescending(u => u.AddedToStoreDate).Where((u, index) => index < 10);
+            return _db.Products.OrderByDescending(u => u.AddedToStoreDate).Take(10);
         }
 
         public IEnumerable<Product> SearchProducts(string query)
         {
             return _db.Products
-                .AsEnumerable()
-                .Where((u) => u.Name.ToLower().Contains(query.ToLower()))
-                .Where((u, index) => index < 10);
+                .Where((u) => u.Name.Contains(query))
+                .Take(10);
         }
 
-        public IEnumerable<Product> FullSearchProducts(string? query, int[]? filter, int? page)
+        public IEnumerable<Product> FullSearchProducts(string? query, int? productTypeId, int? page)
         {
             int itemsPerPage = 5;
 
-            return _db.Products
-                        .ToList()
-                        .Where((u) => MatchesFilter(filter, u) && MatchesQuery(query, u))
-                        .Where((u, index) => IsInPage(page, index, itemsPerPage)
-                        );
-        }
+            IQueryable<Product> products = _db.Products;
 
-        private bool MatchesFilter(int[]? filter, Product u)
-        {
-            return filter != null && filter.Length != 0 ? filter.Contains(u.ProductTypeId) : true;
-        }
+            if (productTypeId != null && productTypeId > 0)
+            {
+                products = products.Where((u) => u.ProductTypeId == productTypeId);
+            }
 
-        private bool MatchesQuery(string? query, Product u)
-        {
-            return query != null && query != "" ?
-                    u.Name.Trim().ToLower().Contains(query.ToLower()) :
-                    true;
-        }
+            if (query != null && query != "")
+            {
+                products = products.Where((u) => u.Name.Contains(query));
+            }
 
-        private bool IsInPage(int? page, int index, int itemsPerPage)
-        {
-            return page != null && page > 1 ?
-                            index >= ((page - 1) * itemsPerPage) && index < page * itemsPerPage :
-                            index < itemsPerPage;
+            if (page != null && page > 1)
+            {
+                products = products.Skip((int.Parse(page.ToString()) - 1) * itemsPerPage);
+            }
+
+            products = products.Take(itemsPerPage);
+
+            return products;
         }
     }
 }
