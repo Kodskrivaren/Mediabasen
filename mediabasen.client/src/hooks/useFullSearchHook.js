2336d2c6bc5ac410cb1e7769a7f8abe21b0d061c
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-07-29 17:17:32 +0200

    hook for full search page

---
+++ mediabasen.client/src/hooks/useFullSearchHook.js
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import productService from "../services/productService";
+import searchHelper from "../utils/searchHelper";
+
+export default function useFullSearchHook({
+  params,
+  setResult,
+  setProductTypes,
+}) {
+  useEffect(() => {
+    async function searchProducts() {
+      const query = params.get(searchHelper.searchQueries.query);
+      const productTypeId = params.get(
+        searchHelper.searchQueries.productTypeId
+      );
+      const page = params.get(searchHelper.searchQueries.page);
+
+      const result = await productService.fullSearchProducts(
+        query,
+        productTypeId,
+        page
+      );
+
+      if (result) {
+        setResult(result.products);
+      }
+    }
+
+    searchProducts();
+  }, [params]);
+
+  useEffect(() => {
+    async function getProductTypes() {
+      const data = await productService.getProductTypes();
+
+      setProductTypes(data);
+    }
+
+    getProductTypes();
+  }, []);
+}
