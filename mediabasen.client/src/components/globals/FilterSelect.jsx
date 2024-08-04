cca0a7321af5509558034cd28f0545337e7a081d
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-08-04 15:39:43 +0200

    created global filter select

---
+++ mediabasen.client/src/components/globals/FilterSelect.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function FilterSelect({ value, onChange, children, disabled }) {
+  return (
+    <select
+      className="bg-dark rounded-lg p-3 text-white flex flex-wrap justify-center gap-x-3"
+      value={value}
+      onChange={onChange}
+      disabled={disabled !== undefined ? disabled : false}>
+      {children}
+    </select>
+  );
+}
