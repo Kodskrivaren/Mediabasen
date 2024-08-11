4af4002e22672974b382de175c906df541af0563
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-08-11 09:03:22 +0200

    added links to contact and about us pages

--- mediabasen.client/src/components/globals/Footer.jsx
+++ mediabasen.client/src/components/globals/Footer.jsx
@@ -1,15 +1,21 @@
+import Link from "./Link";
+
 export default function Footer() {
   const date = new Date();
 
   return (
-    <footer className="bg-dark text-white">
+    <footer className="bg-dark text-white px-2">
       <section className="flex justify-between pt-5 pb-5 mx-auto max-w-7xl">
         <ul>
-          <li>Om oss</li>
-          <li>Kontakt</li>
+          <li>
+            <Link to={"/aboutus"}>Om oss</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>Kontakt</Link>
+          </li>
         </ul>
         <p className="block my-auto">
-          Copyright © {date.getFullYear()} Mediabasen
+          Copyright © {date.getFullYear()} Filip Blomqvist
         </p>
         <section>
           <img />
