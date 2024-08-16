be6938a300f00d2295c41dd221fbf2e45e678a51
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-08-16 17:22:17 +0200

    added line for products list

--- mediabasen.client/src/components/globals/ProductsList.jsx
+++ mediabasen.client/src/components/globals/ProductsList.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ProductXScroll from "./ProductXScroll";
 
 export default function ProductsList({ title, products }) {
   return (
     products &&
     products.length > 0 && (
-      <section className="bg-middle p-3 relative">
-        <h2 className="text-white pb-3 font-bold">{title}</h2>
+      <section className="bg-middle p-3 relative before:block before:w-full before:h-1 before:mb-3 before:bg-line">
+        <h2 className="text-white pb-3 font-bold ">{title}</h2>
         <ProductXScroll {...{ products }} />
       </section>
     )
