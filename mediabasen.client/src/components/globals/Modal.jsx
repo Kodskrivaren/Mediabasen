badef01606630e030515ff338925a112ee928269
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-08-17 16:30:56 +0200

    changed z index

--- mediabasen.client/src/components/globals/Modal.jsx
+++ mediabasen.client/src/components/globals/Modal.jsx
@@ -10,7 +10,7 @@ export default function Modal(props) {
   return (
     <div
       onClick={onClick}
-      className="gmodal fixed flex justify-center items-center flex-col top-0 bottom-0 left-0 right-0 bg-primary bg-opacity-50">
+      className="gmodal fixed flex justify-center items-center flex-col top-0 bottom-0 left-0 right-0 z-50 bg-primary bg-opacity-50">
       <div className="flex flex-col gap-4 max-w-96 text-white rounded h-fit p-8 bg-dark shadow-modal shadow-black">
         {props.children}
       </div>
