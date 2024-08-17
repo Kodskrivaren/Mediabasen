18b2b47a080dd5087e6d26b8c5e84931c31e08bf
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-08-17 16:31:56 +0200

    added page for user details with ability to delete account

---
+++ mediabasen.client/src/components/pages/User/UserDetails.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import Button from "../../globals/Button";
+import DeleteUserModal from "./DeleteUserModal";
+
+export default function UserDetails() {
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+  return (
+    <section className="p-3 flex flex-col gap-y-3">
+      <h2 className="text-2xl font-bold text-center">Konto</h2>
+      <Button
+        classNameColor="bg-red-500"
+        className="mx-auto"
+        onClick={() => setShowDeleteModal(true)}>
+        Ta bort konto
+      </Button>
+      {showDeleteModal && <DeleteUserModal {...{ setShowDeleteModal }} />}
+    </section>
+  );
+}
