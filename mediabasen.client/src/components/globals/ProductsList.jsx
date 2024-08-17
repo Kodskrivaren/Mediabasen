3945118c75b7d1c8dbfd093ac1f9954eae826e97
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-08-17 10:05:06 +0200

    put load spinner on products list

--- mediabasen.client/src/components/globals/ProductsList.jsx
+++ mediabasen.client/src/components/globals/ProductsList.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ProductXScroll from "./ProductXScroll";
+import LoadSpinner from "./LoadSpinner";
 
 export default function ProductsList({ title, products }) {
   return (
-    products &&
-    products.length > 0 && (
-      <section className="bg-middle p-3 relative before:block before:w-full before:h-1 before:mb-3 before:bg-line">
-        <h2 className="text-white pb-3 font-bold ">{title}</h2>
+    <section className="bg-middle p-3 relative before:block before:w-full before:h-1 before:mb-3 before:bg-line">
+      <h2 className="text-white pb-3 font-bold ">{title}</h2>
+      {products !== undefined ? (
         <ProductXScroll {...{ products }} />
-      </section>
-    )
+      ) : (
+        <LoadSpinner className={"mx-auto"} />
+      )}
+    </section>
   );
 }
