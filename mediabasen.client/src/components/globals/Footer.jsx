246a71cc1c5bb6d883370a3dab240e80ee4e5c04
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-08-17 11:34:36 +0200

    added github logo to footer and link to github repository

--- mediabasen.client/src/components/globals/Footer.jsx
+++ mediabasen.client/src/components/globals/Footer.jsx
@@ -1,12 +1,13 @@
 import Link from "./Link";
+import GithubLogo from "../../assets/icons/github-mark-white.png";
 
 export default function Footer() {
   const date = new Date();
 
   return (
     <footer className="bg-dark text-white px-2">
-      <section className="flex justify-between pt-5 pb-5 mx-auto max-w-7xl">
-        <ul>
+      <section className="flex align-middle gap-y-10 justify-center pt-5 pb-5 mx-auto max-w-7xl flex-col md:flex-row md:justify-between">
+        <ul className="flex flex-col flex-grow-0 gap-y-2 md:gap-y-1 w-fit mx-auto md:mx-0">
           <li>
             <Link to={"/aboutus"}>Om oss</Link>
           </li>
@@ -14,12 +15,15 @@ export default function Footer() {
             <Link to={"/contact"}>Kontakt</Link>
           </li>
         </ul>
-        <p className="block my-auto">
+        <p className="block my-auto w-fit mx-auto md:mx-0">
           Copyright © {date.getFullYear()} Filip Blomqvist
         </p>
-        <section>
-          <img />
-          <img />
+        <section className="flex-grow-0 w-fit mx-auto md:mx-0">
+          <Link
+            to={"https://github.com/Kodskrivaren/Mediabasen"}
+            target={"_blank"}>
+            <img src={GithubLogo} className="w-16 h-16 mx-auto" />
+          </Link>
         </section>
       </section>
     </footer>
