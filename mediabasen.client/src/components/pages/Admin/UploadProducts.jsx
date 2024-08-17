7db832306b8a33f7d11b9500aa8f62b53fc5022a
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-08-17 10:08:36 +0200

    added load spinner after upload file click

--- mediabasen.client/src/components/pages/Admin/UploadProducts.jsx
+++ mediabasen.client/src/components/pages/Admin/UploadProducts.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import Button from "../../globals/Button";
 import productService from "../../../services/productService";
+import LoadSpinner from "../../globals/LoadSpinner";
 
 export default function UploadProducts() {
   const [file, setFile] = useState();
   const [serverMessage, setServerMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
   async function onSubmit(e) {
     e.preventDefault();
     setServerMessage("");
+    setLoading(true);
     const response = await productService.uploadProducts(file);
 
     if (response) {
@@ -16,6 +19,7 @@ export default function UploadProducts() {
     } else {
       setServerMessage("Något gick fel...");
     }
+    setLoading(false);
   }
 
   function onFileChange(e) {
@@ -26,8 +30,8 @@ export default function UploadProducts() {
     <form className="text-white flex flex-col gap-y-3" onSubmit={onSubmit}>
       <h3>Ladda upp produkter</h3>
       <input type="file" onChange={onFileChange} />
-      <Button type="submit" disabled={file === undefined}>
-        Ladda upp
+      <Button type="submit" disabled={file === undefined || loading}>
+        {loading ? <LoadSpinner className={"mx-auto h-8 w-8"} /> : "Ladda upp"}
       </Button>
       <p>{serverMessage}</p>
     </form>
