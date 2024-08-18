117624c6f668cfdf4bf9a50b49c9fb4f57ab332c
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2024-08-18 13:26:10 +0200

    added options for setting class names and max width

--- mediabasen.client/src/components/globals/Modal.jsx
+++ mediabasen.client/src/components/globals/Modal.jsx
@@ -11,7 +11,12 @@ export default function Modal(props) {
     <div
       onClick={onClick}
       className="gmodal fixed flex justify-center items-center flex-col top-0 bottom-0 left-0 right-0 z-50 bg-primary bg-opacity-50">
-      <div className="flex flex-col gap-4 max-w-96 text-white rounded h-fit p-8 bg-dark shadow-modal shadow-black">
+      <div
+        className={`flex flex-col gap-4 ${
+          props.maxWidth ? props.maxWidth : "max-w-96"
+        } text-white rounded h-fit p-8 bg-dark shadow-modal shadow-black ${
+          props.className ? props.className : ""
+        }`}>
         {props.children}
       </div>
     </div>
