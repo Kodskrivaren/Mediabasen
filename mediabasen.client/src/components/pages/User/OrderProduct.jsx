befd7409a13041b6be074c65e60e6a9b7ca49351
Kodskrivaren <filip.blomqvist.07@gmail.com>
2024-09-09 17:34:08 +0200

    style change and changed displayed text

--- mediabasen.client/src/components/pages/User/OrderProduct.jsx
+++ mediabasen.client/src/components/pages/User/OrderProduct.jsx
@@ -5,7 +5,7 @@ export default function OrderProduct({ order, item, index }) {
   return (
     <li
       key={`item-${item.id}`}
-      className={`${index === 0 ? "mt-3" : ""} ${
+      className={`${index === 0 ? "mt-3" : "mt-2"} ${
         index === order.orderItems.length - 1 ? "mb-3" : ""
       } flex gap-x-3`}>
       {item.product.images && item.product.images.length > 0 ? (
@@ -15,8 +15,8 @@ export default function OrderProduct({ order, item, index }) {
       )}
       <section className="w-2/3">
         <h5 className="font-bold">{item.product.name}</h5>
-        <p>Antal: {item.amount}</p>
-        <p>Pris: {item.price}</p>
+        <p>{item.amount} x</p>
+        <p>{item.price} kr</p>
       </section>
     </li>
   );
