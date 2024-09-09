17f93a488d23606bb96758e22fc086bc16c111da
Kodskrivaren <filip.blomqvist.07@gmail.com>
2024-09-09 17:34:32 +0200

    style changes and changed displayed text

--- mediabasen.client/src/components/pages/User/UserOrder.jsx
+++ mediabasen.client/src/components/pages/User/UserOrder.jsx
@@ -5,27 +5,36 @@ export default function UserOrder({ order }) {
   const [extended, setExtended] = useState(false);
 
   return (
-    <li className="bg-dark rounded">
-      <section className="p-3 bg-black" onClick={() => setExtended(!extended)}>
-        <h4>Order: {order.id}</h4>
-        <p>Order lagd: {new Date(order.orderPlaced).toLocaleString()}</p>
-        <p>
-          Order skickad:{" "}
-          {order.orderShipped
-            ? new Date(order.orderShipped).toLocaleString()
-            : "Ej skickad!"}
-        </p>
-        <p>Antal: {order.orderItems.length}</p>
+    <li className="bg-dark rounded mb-3">
+      <section className="p-3" onClick={() => setExtended(!extended)}>
+        <h4>{order.id}</h4>
+        <div className="flex justify-between mt-3">
+          <p>{new Date(order.orderPlaced).toLocaleDateString()}</p>
+          <p>{order.orderShipped ? "Skickad" : "Behandlas"}</p>
+          <p>{order.totalPrice} kr</p>
+        </div>
       </section>
       <div
-        className={`grid transition-[grid-template-rows] ${
+        className={`grid transition-[grid-template-rows] bg-light ${
           extended ? "grid-rows-open" : "grid-rows-closed"
         }`}>
-        <ul className="mx-3 overflow-hidden flex flex-col gap-y-2">
-          {order.orderItems.map((item, index) => (
-            <OrderProduct key={`item-${item.id}`} {...{ order, item, index }} />
-          ))}
-        </ul>
+        <div className="mx-3 overflow-hidden flex flex-col gap-y-2">
+          <p className="mt-3">
+            Order skickad:{" "}
+            {order.orderShipped
+              ? new Date(order.orderShipped).toLocaleString()
+              : "Ej skickad!"}
+          </p>
+          <p>Antal: {order.orderItems.length}</p>
+          <ul>
+            {order.orderItems.map((item, index) => (
+              <OrderProduct
+                key={`item-${item.id}`}
+                {...{ order, item, index }}
+              />
+            ))}
+          </ul>
+        </div>
       </div>
     </li>
   );
