959f5e1dc676e7f3a3b0978f84c831d341e1cb43
Kodskrivaren <filip.blomqvist.07@gmail.com>
2024-09-12 13:09:58 +0200

    created service for getting cart

---
+++ Mediabasen.Server/Services/CartService.cs
@@ -0,0 +1,23 @@
+﻿using Mediabasen.DataAccess.Repository.IRepository;
+using Mediabasen.Models.Cart;
+
+namespace Mediabasen.Server.Services
+{
+    public class CartService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CartService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Cart GetCart(string cartId, bool tracked = false)
+        {
+            if (cartId == null || cartId == "") return null;
+
+            int parsedCartId = int.Parse(cartId);
+
+            return _unitOfWork.Cart.GetFirstOrDefault(u => u.Id == parsedCartId, includeProperties: "CartProducts", tracked: tracked);
+        }
+    }
+}
