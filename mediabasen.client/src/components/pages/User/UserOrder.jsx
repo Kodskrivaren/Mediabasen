5485b7f657307ea37a9147c4630db1f9da719adf
Kodskrivaren <filip.blomqvist.07@gmail.com>
2024-09-13 16:49:04 +0200

    added button to pay an order

--- mediabasen.client/src/components/pages/User/UserOrder.jsx
+++ mediabasen.client/src/components/pages/User/UserOrder.jsx
@@ -1,8 +1,22 @@
 import React, { useState } from "react";
 import OrderProduct from "./OrderProduct";
+import Button from "../../globals/Button";
+import orderService from "../../../services/orderService";
+import LoadSpinner from "../../globals/LoadSpinner";
 
 export default function UserOrder({ order }) {
   const [extended, setExtended] = useState(false);
+  const [loadingPaymentUrl, setLoadingPaymentUrl] = useState(false);
+
+  async function payOrderClick() {
+    setLoadingPaymentUrl(true);
+    const result = await orderService.payOrder(order.id);
+
+    if (result) {
+      window.location = result.stripeUrl;
+    }
+    setLoadingPaymentUrl(false);
+  }
 
   return (
     <li className="bg-dark rounded mb-3">
@@ -10,7 +24,13 @@ export default function UserOrder({ order }) {
         <h4>{order.id}</h4>
         <div className="flex justify-between mt-3">
           <p>{new Date(order.orderPlaced).toLocaleDateString()}</p>
-          <p>{order.orderShipped ? "Skickad" : "Behandlas"}</p>
+          <p>
+            {!order.paid
+              ? "Reserverad"
+              : order.orderShipped
+              ? "Skickad"
+              : "Behandlas"}
+          </p>
           <p>{order.totalPrice} kr</p>
         </div>
       </section>
@@ -20,10 +40,26 @@ export default function UserOrder({ order }) {
         }`}>
         <div className="mx-3 overflow-hidden flex flex-col gap-y-2">
           <p className="mt-3">
-            Order skickad:{" "}
+            Skickad:{" "}
             {order.orderShipped
               ? new Date(order.orderShipped).toLocaleString()
               : "Ej skickad!"}
+          </p>
+          <p>
+            Betald: {order.paid ? "Ja" : "Nej"}{" "}
+            {!order.paid && (
+              <Button
+                classNameColor="bg-accent"
+                className="mr-0 ml-auto"
+                disabled={loadingPaymentUrl}
+                onClick={payOrderClick}>
+                {loadingPaymentUrl ? (
+                  <LoadSpinner className={"w-6 h-6 mx-auto"} />
+                ) : (
+                  "Betala"
+                )}
+              </Button>
+            )}
           </p>
           <p>Antal: {order.orderItems.length}</p>
           <ul>
