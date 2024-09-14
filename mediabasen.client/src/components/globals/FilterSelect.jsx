35dd4363ef4965d2b9a170c1c75791aaa0a9dc6d
Kodskrivaren <filip.blomqvist.07@gmail.com>
2024-09-14 09:47:22 +0200

    added hover effect

--- mediabasen.client/src/components/globals/FilterSelect.jsx
+++ mediabasen.client/src/components/globals/FilterSelect.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function FilterSelect({ value, onChange, children, disabled }) {
   return (
     <select
-      className="bg-dark rounded-lg p-3 text-white flex flex-wrap justify-center gap-x-3"
+      className="bg-dark rounded-lg p-3 text-white flex flex-wrap justify-center gap-x-3 transition-colors hover:bg-black"
       value={value}
       onChange={onChange}
       disabled={disabled !== undefined ? disabled : false}>
