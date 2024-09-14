0195fdd38918e9a039807d5155cd6dd553997b0b
Kodskrivaren <filip.blomqvist.07@gmail.com>
2024-09-14 09:05:54 +0200

    added width to prevent layout change when removing products

--- mediabasen.client/src/components/pages/Cart/ProductList.jsx
+++ mediabasen.client/src/components/pages/Cart/ProductList.jsx
@@ -3,7 +3,7 @@ import CartProduct from "./CartProduct";
 
 export default function ProductList({ products }) {
   return (
-    <ul className="flex flex-col gap-y-5">
+    <ul className="flex flex-col gap-y-5 md:w-1/2">
       {products.map(({ id, product, count }) => (
         <CartProduct key={`cart-product-${id}`} {...{ id, product, count }} />
       ))}
