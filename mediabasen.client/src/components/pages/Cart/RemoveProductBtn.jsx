863fc100fcabb8e0ab04b8fb2a7fd85f9e998083
Kodskrivaren <filip.blomqvist.07@gmail.com>
2024-09-15 10:56:12 +0200

    style changes and added trash can icon

--- mediabasen.client/src/components/pages/Cart/RemoveProductBtn.jsx
+++ mediabasen.client/src/components/pages/Cart/RemoveProductBtn.jsx
@@ -3,6 +3,7 @@ import cartService from "../../../services/cartService";
 import LoadSpinner from "../../globals/LoadSpinner";
 import CartContext from "../../../contexts/CartContext";
 import ButtonSecondary from "../../globals/ButtonSecondary";
+import TrashCan from "../../../assets/icons/trash-can.svg?react";
 
 export default function RemoveProductBtn({ id }) {
   const { setCart } = useContext(CartContext);
@@ -18,13 +19,13 @@ export default function RemoveProductBtn({ id }) {
 
   return (
     <ButtonSecondary
-      className="mt-auto"
+      className="mt-auto bg-middle absolute top-1 right-1 w-fit"
       disabled={removeClickLoading}
       onClick={removeProduct}>
       {removeClickLoading ? (
-        <LoadSpinner className={"mx-auto w-8 h-8"} />
+        <LoadSpinner className={"mx-auto w-6 h-6"} />
       ) : (
-        "Ta bort"
+        <TrashCan className="w-6 h-6 mx-auto" />
       )}
     </ButtonSecondary>
   );
