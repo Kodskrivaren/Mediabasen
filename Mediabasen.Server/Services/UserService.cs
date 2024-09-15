fc1debe03564c93feeec40166953944f2ac4b97c
Kodskrivaren <filip.blomqvist.07@gmail.com>
2024-09-15 10:51:29 +0200

    added method for getting users email by id

--- Mediabasen.Server/Services/UserService.cs
+++ Mediabasen.Server/Services/UserService.cs
@@ -1,7 +1,26 @@
-﻿namespace Mediabasen.Server.Services
+﻿using Mediabasen.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Mediabasen.Server.Services
 {
     public class UserService
     {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public UserService(SignInManager<ApplicationUser> signInManager) 
+        { 
+            _signInManager = signInManager; 
+        }
+
+        public string GetUserEmailById(string userId)
+        {
+            var user = _signInManager.UserManager.FindByIdAsync(userId).GetAwaiter().GetResult();
+
+            if (user == null) { return ""; }
+
+            return user.Email;
+        }
+
         public string GetUserId(HttpContext httpContext)
         {
             var idClaim = httpContext.User.Claims.FirstOrDefault(u => u.ToString().Contains("nameidentifier"));
