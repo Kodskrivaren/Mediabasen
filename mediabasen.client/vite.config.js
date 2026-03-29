42e44b60cbd772ed19eb50e6736599d63b5a0d83
Filip Blomqvist <filip.blomqvist.07@gmail.com>
2026-03-29 13:06:45 +0200

    removed logic for cert used in development

--- mediabasen.client/vite.config.js
+++ mediabasen.client/vite.config.js
@@ -3,46 +3,13 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import plugin from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import fs from "fs";
-import path from "path";
-import child_process from "child_process";
 import { env } from "process";
-
-const baseFolder =
-  env.APPDATA !== undefined && env.APPDATA !== ""
-    ? `${env.APPDATA}/ASP.NET/https`
-    : `${env.HOME}/.aspnet/https`;
-
-const certificateName = "mediabasen.client";
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
-
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-  if (
-    0 !==
-    child_process.spawnSync(
-      "dotnet",
-      [
-        "dev-certs",
-        "https",
-        "--export-path",
-        certFilePath,
-        "--format",
-        "Pem",
-        "--no-password",
-      ],
-      { stdio: "inherit" }
-    ).status
-  ) {
-    throw new Error("Could not create certificate.");
-  }
-}
 
 const target = env.ASPNETCORE_HTTPS_PORT
   ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
   : env.ASPNETCORE_URLS
-  ? env.ASPNETCORE_URLS.split(";")[0]
-  : "https://localhost:7121";
+    ? env.ASPNETCORE_URLS.split(";")[0]
+    : "https://localhost:7121";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -64,9 +31,5 @@ export default defineConfig({
       },
     },
     port: 5173,
-    https: {
-      key: fs.readFileSync(keyFilePath),
-      cert: fs.readFileSync(certFilePath),
-    },
   },
 });
